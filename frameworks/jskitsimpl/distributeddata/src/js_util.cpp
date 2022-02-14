cc96ae1b54806ba9573b8d2ebb4818fa5ec20a85
wufengshan <wufengshan@huawei.com>
2022-02-14 21:32:34 +0800

    Signed-off-by: wufengshan <wufengshan@huawei.com>
     Changes to be committed:
    	modified:   frameworks/jskitsimpl/distributeddata/src/js_util.cpp

--- frameworks/jskitsimpl/distributeddata/src/js_util.cpp
+++ frameworks/jskitsimpl/distributeddata/src/js_util.cpp
@@ -292,22 +292,19 @@ napi_value JSUtil::Convert2JSNumber(napi_env env, const std::vector<uint8_t> &da
     double value;
     switch (data[0]) {
         case INTEGER:
-            int err = memcpy_s(byteValue, sizeof(byteValue), data.data() + DATA_POS, sizeof(int32_t));
-            if (err != EOK) {
+            if (memcpy_s(byteValue, sizeof(byteValue), data.data() + DATA_POS, sizeof(int32_t)) != EOK) {
                 return nullptr;
             }
             value = *(reinterpret_cast<const int32_t *>(byteValue));
             break;
         case FLOAT:
-            int err_ = memcpy_s(byteValue, sizeof(byteValue), data.data() + DATA_POS, sizeof(float));
-            if (err_ != EOK) {
+            if (memcpy_s(byteValue, sizeof(byteValue), data.data() + DATA_POS, sizeof(float)) != EOK) {
                 return nullptr;
             }
             value = *(reinterpret_cast<const float *>(byteValue));
             break;
         case DOUBLE:
-            int result = memcpy_s(byteValue, sizeof(byteValue), data.data() + DATA_POS, sizeof(double));
-            if (result != EOK) {
+            if (memcpy_s(byteValue, sizeof(byteValue), data.data() + DATA_POS, sizeof(double)) != EOK) {
                 return nullptr;
             }
             value = *(reinterpret_cast<const double *>(byteValue));
