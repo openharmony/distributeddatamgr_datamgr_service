92bca063de39c9a92cc31251408209c983944afb
yangliu <yangliu146@huawei.com>
2022-05-20 20:43:02 +0800

    modify code
    Signed-off-by: yangliu <yangliu146@huawei.com>

--- frameworks/jskitsimpl/distributeddata/include/js_util.h
+++ frameworks/jskitsimpl/distributeddata/include/js_util.h
@@ -22,8 +22,6 @@
 #include "napi/native_api.h"
 #include "napi/native_common.h"
 #include "napi/native_node_api.h"
-#include "datashare_predicates.h"
-#include "datashare_values_bucket.h"
 
 namespace OHOS::DistributedData {
 class JSUtil final {
