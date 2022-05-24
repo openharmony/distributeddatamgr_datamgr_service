7609ce2836eda0c627af66bf19798fc64fbb0390
PaDaBoo <xuejianwu@huawei.com>
2022-05-24 21:07:31 +0800

    fix access of rdb database path for app sanbox
    Signed-off-by: PaDaBoo <xuejianwu@huawei.com>

--- services/distributeddataservice/service/rdb/rdb_syncer.cpp
+++ services/distributeddataservice/service/rdb/rdb_syncer.cpp
@@ -84,7 +84,7 @@ std::string RdbSyncer::GetAppId() const
 
 std::string RdbSyncer::GetPath() const
 {
-    return param_.path_;
+    return param_.realPath_;
 }
 
 std::string RdbSyncer::GetStoreId() const
