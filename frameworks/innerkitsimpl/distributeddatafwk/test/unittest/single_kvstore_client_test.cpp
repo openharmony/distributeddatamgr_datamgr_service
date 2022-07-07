7b23061b31e35f07a49688a580f456d764637ff6
Sven Wang <wanghancai@huawei.com>
2022-07-07 11:36:00 +0800

    fixed code style
    Signed-off-by: Sven Wang <wanghancai@huawei.com>

--- frameworks/innerkitsimpl/distributeddatafwk/test/unittest/single_kvstore_client_test.cpp
+++ frameworks/innerkitsimpl/distributeddatafwk/test/unittest/single_kvstore_client_test.cpp
@@ -266,7 +266,7 @@ HWTEST_F(SingleKvStoreClientTest, GetEmptyEntries, TestSize.Level1)
 {
     EXPECT_NE(singleKvStorePtr, nullptr) << "kvStorePtr is null.";
     std::vector<Entry> results;
-    auto status = singleKvStorePtr->GetEntries({"SUCCESS_TEST"}, results);
+    auto status = singleKvStorePtr->GetEntries({ "SUCCESS_TEST" }, results);
     EXPECT_EQ(status, Status::SUCCESS) << "status is not SUCCESS.";
     EXPECT_EQ(results.size(), 0) << "entries size is not empty.";
 }
