139f3310b5f4c669782757f6f914ace2758aec57
zuojiangjiang <zuojiangjiang@huawei.com>
2022-10-24 20:28:13 +0800

    fix error Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- services/distributeddataservice/service/matrix/src/device_matrix.cpp
+++ services/distributeddataservice/service/matrix/src/device_matrix.cpp
@@ -174,7 +174,7 @@ uint16_t DeviceMatrix::ConvertMask(const std::string &device, uint16_t code)
     uint16_t result = code & META_STORE_MASK;
     code &= ~META_STORE_MASK;
     while (code != 0) {
-        uint16_t index = code & (~(code - 1));
+        uint16_t index = (~code) & (code - 1);
         // 0x6666: 1010101010101010  0x5555: 0101010101010101 1: move the high(0x6666) to low(0x5555) bits
         index = ((index & 0x6666) >> 1) + (index & 0x5555);
         // 0xCCCC: 1100110011001100  0x3333: 0011001100110011 2: the count save at 2 bits
@@ -190,7 +190,7 @@ uint16_t DeviceMatrix::ConvertMask(const std::string &device, uint16_t code)
         auto &app = meta.maskInfo[index];
         for (size_t i = 0; i < maskApps_.size(); i++) {
             if (maskApps_[i] == app) {
-                result |= SetMask(index);
+                result |= SetMask(i);
                 break;
             }
         }
