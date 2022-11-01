4962a3f7b453555b5e6d5cdc7168fa50bdc28bec
zuojiangjiang <zuojiangjiang@huawei.com>
2022-11-01 09:13:56 +0800

    fix error Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- services/distributeddataservice/adapter/include/communicator/device_manager_adapter.h
+++ services/distributeddataservice/adapter/include/communicator/device_manager_adapter.h
@@ -74,7 +74,7 @@ private:
     DeviceInfo localInfo_ {};
     ConcurrentMap<const AppDeviceChangeListener *, const AppDeviceChangeListener *> observers_ {};
     LRUBucket<std::string, DeviceInfo> deviceInfos_ {64};
-    static constexpr size_t TIME_TASK_CAPACITY = 4;
+    static constexpr size_t TIME_TASK_CAPACITY = 50;
     KvScheduler scheduler_ {TIME_TASK_CAPACITY};
     static constexpr int32_t SYNC_TIMEOUT = 30 * 1000; // ms
     ConcurrentMap<std::string, std::string> syncTask_ {};
