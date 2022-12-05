83d3dc8a0f220bd93e79cb839c6f00d085b4eadf
zuojiangjiang <zuojiangjiang@huawei.com>
2022-12-05 14:09:10 +0800

    fix error Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- services/distributeddataservice/service/object/object_common.h
+++ services/distributeddataservice/service/object/object_common.h
@@ -17,7 +17,7 @@
 #define DISTRIBUTEDDATAMGR_OBJECT_COMMON_H
 namespace OHOS {
 namespace DistributedObject {
-enum ObjectDistributedType {
+enum class ObjectDistributedType {
     OBJECT_SINGLE_VERSION = 20,
     DISTRIBUTED_TYPE_BUTT
 };
