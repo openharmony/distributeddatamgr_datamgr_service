438a05f354385f2764fa95bfdf8b1828dc630bfd
dboy190 <dulei1@huawei.com>
2022-12-08 16:33:57 +0800

    modify member name
    Signed-off-by: dboy190 <dulei1@huawei.com>

--- services/distributeddataservice/app/src/kvstore_data_service.h
+++ services/distributeddataservice/app/src/kvstore_data_service.h
@@ -121,8 +121,8 @@ private:
 
     static constexpr int TEN_SEC = 10;
 
-    std::mutex clientDeathObserverMutex_;
-    std::map<uint32_t, KvStoreClientDeathObserverImpl> clientDeathObserverMap_;
+    std::mutex mutex_;
+    std::map<uint32_t, KvStoreClientDeathObserverImpl> clients_;
     std::shared_ptr<KvStoreAccountObserver> accountEventObserver_;
 
     std::shared_ptr<Security> security_;
