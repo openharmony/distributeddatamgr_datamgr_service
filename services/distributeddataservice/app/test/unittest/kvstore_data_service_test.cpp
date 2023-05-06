d16eb4528dc023f5803fbf6a15e30d84060e73eb
renjiecui <cuirenjie@huawei.com>
2023-05-06 14:32:34 +0800

    add ExtcutorPool and modify code
    Signed-off-by: renjiecui <cuirenjie@huawei.com>

--- services/distributeddataservice/app/test/unittest/kvstore_data_service_test.cpp
+++ services/distributeddataservice/app/test/unittest/kvstore_data_service_test.cpp
@@ -57,7 +57,7 @@ HWTEST_F(KvStoreDataServiceTest, RegisterClientDeathObserver001, TestSize.Level1
     KvStoreDataService kvDataService;
     Bootstrap::GetInstance().LoadComponents();
     Bootstrap::GetInstance().LoadCheckers();
-    KvStoreMetaManager::GetInstance().InitMetaParameter();
+    KvStoreMetaManager::GetInstance().InitMetaParameter(std::make_shared<ExecutorPool>(12, 5));
     Status status = kvDataService.RegisterClientDeathObserver(appId, new KvStoreClientDeathObserver());
     EXPECT_EQ(status, Status::SUCCESS) << "RegisterClientDeathObserver failed";
 }
