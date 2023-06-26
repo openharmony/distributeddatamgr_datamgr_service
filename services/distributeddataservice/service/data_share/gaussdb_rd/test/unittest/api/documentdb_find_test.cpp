f57da5d6de502ac1d92a85b85c49a4b70fee7ef1
mazhao <mazhao12@huawei.com>
2023-06-26 21:55:25 +0800

    fix format
    Signed-off-by: mazhao <mazhao12@huawei.com>

--- services/distributeddataservice/service/data_share/gaussdb_rd/test/unittest/api/documentdb_find_test.cpp
+++ services/distributeddataservice/service/data_share/gaussdb_rd/test/unittest/api/documentdb_find_test.cpp
@@ -79,17 +79,6 @@ static const char *g_document20 = "{\"_id\" : \"20\", \"name\":\"doc20\",\"ITEM\
 static const char *g_document23 = "{\"_id\" : \"23\", \"name\":\"doc22\",\"ITEM\" : "
                                   "true,\"personInfo\":[{\"school\":\"b\", \"age\":15}, [{\"school\":\"doc23\"}, 10, "
                                   "{\"school\":\"doc23\"}, true, {\"school\":\"y\"}], {\"school\":\"b\"}]}";
-static const char *document0631 = R""({"_id":"city_11_com.acts.ohos.data.datasharetestclient_100",
-      "bundleName":"com.acts.ohos.data.datasharetestclient","key":"city","subscriberId":11,
-      "timestamp":1509100700,"userId":100,"value":{"type":1,"value":"xian"},"version":0})"";
-static const char *document0632 = R""({"_id":"datashareproxy://com.acts.ohos.data.datasharetest/appInfo_11_com.acts.oh
-    os.data.datasharetest_100","bundleName":"com.acts.ohos.data.datasharetest","key":"datashareproxy://com.acts.ohos
-    .data.datasharetest/appInfo","subscriberId":11,"timestamp":1509100700,"userId":100,
-    "value":{"type":1,"value":"\"Qing\""},"version":0})"";
-static const char *document0633 =
-    R""({"_id":"empty_11_com.acts.ohos.data.datasharetestclient_100","bundleName":"com.acts.ohos.data.datasharetestcl
-    ient","key":"empty","subscriberId":11,"timestamp":1509100700,"userId":100,"value":{"type":1,"value":"nobody sub"},
-    "version":0})"";
 static std::vector<const char *> g_data = { g_document1, g_document2, g_document3, g_document4, g_document5,
     g_document6, g_document7, g_document8, g_document9, g_document10, g_document11, g_document12, g_document13,
     g_document14, g_document15, g_document16, g_document17, g_document18, g_document19, g_document20, g_document23 };
@@ -1484,11 +1473,6 @@ HWTEST_F(DocumentDBFindTest, DocumentDBFindTest063, TestSize.Level1)
     int status = GRD_DBOpen(path.c_str(), nullptr, GRD_DB_OPEN_CREATE, &test_db);
     EXPECT_EQ(status, GRD_OK);
     EXPECT_EQ(GRD_CreateCollection(test_db, colName, "", 0), GRD_OK);
-
-    EXPECT_EQ(GRD_InsertDoc(test_db, colName, document0631, 0), GRD_OK);
-    EXPECT_EQ(GRD_InsertDoc(test_db, colName, document0632, 0), GRD_OK);
-    EXPECT_EQ(GRD_InsertDoc(test_db, colName, document0633, 0), GRD_OK);
-
     string document1 = "{\"_id\":\"key2_11_com.acts.ohos.data.datasharetestclient_100\",\
         \"bundleName\":\"com.acts.ohos.data.datasharetestclient\",\"key\":\"key2\",\
         \"subscriberId\":11,\"timestamp\":1509100700,""\"userId\":100,\"value\":{\"type\":0,";
