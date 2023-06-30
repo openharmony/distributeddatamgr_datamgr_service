9bed365c05a89596e5fcbc117fc722f037c40387
zuojiangjiang <zuojiangjiang@huawei.com>
2023-06-30 19:44:25 +0800

    微秒统一ms Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- services/distributeddataservice/service/cloud/cloud_service_impl.h
+++ services/distributeddataservice/service/cloud/cloud_service_impl.h
@@ -66,6 +66,7 @@ private:
     static constexpr int32_t RETRY_TIMES = 10;
     static constexpr int32_t RETRY_INTERVAL = 30;
     static constexpr int32_t EXPIRE_INTERVAL = 7 * 24; // 7 day
+    static constexpr int32_t TO_MS = 1000;
 
     bool UpdateCloudInfo(int32_t user);
     bool UpdateSchema(int32_t user);
