2934b36b0f1e588c4094c87465e73c16dee3c03c
wanghuajian <wanghuajian3@huawei.com>
2023-08-02 15:59:51 +0800

    fix autosync failed when uninstall notepad
    Signed-off-by: wanghuajian <wanghuajian3@huawei.com>

--- services/distributeddataservice/service/cloud/cloud_service_impl.cpp
+++ services/distributeddataservice/service/cloud/cloud_service_impl.cpp
@@ -464,7 +464,6 @@ CloudInfo CloudServiceImpl::GetCloudInfo(int32_t userId)
 int32_t CloudServiceImpl::OnAppUninstall(
     const std::string &bundleName, int32_t user, int32_t index)
 {
-    uint32_t tokenId;
     std::string prefix = StoreMetaData::GetPrefix(
         { DeviceManagerAdapter::GetInstance().GetLocalDevice().uuid, std::to_string(user), "default", bundleName });
     std::vector<StoreMetaData> storeMetaData;
@@ -474,15 +473,12 @@ int32_t CloudServiceImpl::OnAppUninstall(
     }
     for (auto &meta : storeMetaData) {
         if (meta.instanceId == index && !meta.appId.empty() && !meta.storeId.empty()) {
-            ZLOGI("uninstalled bundleName:%{public}s stordId:%{public}s", bundleName.c_str(),
-                Anonymous::Change(meta.storeId).c_str());
-            tokenId = meta.tokenId;
+            AutoCache::GetInstance().CloseStore(meta.tokenId);
             break;
         }
     }
     MetaDataManager::GetInstance().DelMeta(Subscription::GetRelationKey(user, bundleName), true);
     MetaDataManager::GetInstance().DelMeta(CloudInfo::GetSchemaKey(user, bundleName, index), true);
-    AutoCache::GetInstance().CloseStore(tokenId);
     return E_OK;
 }
 
