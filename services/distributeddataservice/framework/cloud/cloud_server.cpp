9f341e82448d4b3622204b0ff64ec71206d597b5
zuojiangjiang <zuojiangjiang@huawei.com>
2023-09-15 17:36:34 +0800

    update Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- services/distributeddataservice/framework/cloud/cloud_server.cpp
+++ services/distributeddataservice/framework/cloud/cloud_server.cpp
@@ -64,7 +64,7 @@ void CloudServer::Clean(int32_t userId)
 {
 }
 
-void OnCloudSwitchChanged(int32_t userId, const std::string &id)
+void CloudServer::OnCloudSwitchChanged(int32_t userId, const std::string &id)
 {
 }
 } // namespace OHOS::DistributedData
