992ef4d2b7fa5521c7d1c3b22c53a8a09f3e29be
z30053452 <zhiyihang1@huawei.com>
2023-11-01 14:20:28 +0800

    fix codecheck
    Signed-off-by: z30053452 <zhiyihang1@huawei.com>

--- services/distributeddataservice/service/udmf/udmf_service_impl.cpp
+++ services/distributeddataservice/service/udmf/udmf_service_impl.cpp
@@ -404,8 +404,9 @@ int32_t UdmfServiceImpl::AddPrivilege(const QueryOption &query, Privilege &privi
     }
 
     if (key.intention == "drag") {
-        auto end = DRAG_AUTHORIZED_PROCESSES + sizeof(DRAG_AUTHORIZED_PROCESSES) / sizeof (DRAG_AUTHORIZED_PROCESSES[0]);
-        if (find(DRAG_AUTHORIZED_PROCESSES, end, processName)) {
+        auto size = std::end(DRAG_AUTHORIZED_PROCESSES) - std::begin(DRAG_AUTHORIZED_PROCESSES);
+        if (find(DRAG_AUTHORIZED_PROCESSES, DRAG_AUTHORIZED_PROCESSES + size, processName) !=
+            DRAG_AUTHORIZED_PROCESSES + size) {
             ZLOGE("Process: %{public}s has no permission to intention: drag", processName.c_str());
             return E_NO_PERMISSION;
         }
