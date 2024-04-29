262aba1e3408afd92b4795faabde2b48fdf5c594
zuojiangjiang <zuojiangjiang@huawei.com>
2024-04-29 17:58:26 +0800

    update Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- services/distributeddataservice/service/kvdb/kvdb_service_impl.cpp
+++ services/distributeddataservice/service/kvdb/kvdb_service_impl.cpp
@@ -285,7 +285,6 @@ Status KVDBServiceImpl::Sync(const AppId &appId, const StoreId &storeId, const S
                 Anonymous::Change(storeId.storeId).c_str());
             return Status::SUCCESS;
         }
-        CloudSync(appId, storeId);
     }
     return KvStoreSyncManager::GetInstance()->AddSyncOperation(uintptr_t(metaData.tokenId), delay,
         std::bind(&KVDBServiceImpl::DoSyncInOrder, this, metaData, syncInfo, std::placeholders::_1, ACTION_SYNC),
@@ -321,6 +320,7 @@ Status KVDBServiceImpl::SyncExt(const AppId &appId, const StoreId &storeId, cons
 
 Status KVDBServiceImpl::NotifyDataChange(const AppId &appId, const StoreId &storeId)
 {
+    CloudSync(appId, storeId);
     StoreMetaData meta = GetStoreMetaData(appId, storeId);
     if (!MetaDataManager::GetInstance().LoadMeta(meta.GetKey(), meta)) {
         ZLOGE("invalid, appId:%{public}s storeId:%{public}s",
