fcf6a14a079f36a7ef2db92c6a55793ff856d1ab
yangliu <yangliu178@huawei.com>
2024-05-19 17:43:53 +0800

    fix
    Signed-off-by: yangliu <yangliu178@huawei.com>

--- services/distributeddataservice/service/kvdb/kvdb_general_store.cpp
+++ services/distributeddataservice/service/kvdb/kvdb_general_store.cpp
@@ -386,14 +386,16 @@ void KVDBGeneralStore::SetEqualIdentifier(const std::string &appId, const std::s
     if (!sameAccountDevs.empty()) {
         auto accountId = AccountDelegate::GetInstance()->GetUnencryptedAccountId();
         auto syncIdentifier = KvManager::GetKvStoreIdentifier(accountId, appId, storeId);
-        ZLOGI("same account set compatible identifier store:%{public}s, user:%{public}s",
-            Anonymous::Change(storeId).c_str(), Anonymous::Change(accountId).c_str());
+        ZLOGI("same account set compatible identifier store:%{public}s, user:%{public}s, device:%{public}.10s",
+            Anonymous::Change(storeId).c_str(), Anonymous::Change(accountId).c_str(),
+            DistributedData::Serializable::Marshall(sameAccountDevs).c_str());
         delegate_->SetEqualIdentifier(syncIdentifier, sameAccountDevs);
     }
     if (!defaultAccountDevs.empty()) {
         auto syncIdentifier = KvManager::GetKvStoreIdentifier(defaultAccountId, appId, storeId);
-        ZLOGI("no account set compatible identifier store:%{public}s, user:%{public}s",
-            Anonymous::Change(storeId).c_str(), Anonymous::Change(accountId).c_str());
+        ZLOGI("no account set compatible identifier store:%{public}s, user:%{public}s, device:%{public}.10s",
+            Anonymous::Change(storeId).c_str(), Anonymous::Change(accountId).c_str(),
+            DistributedData::Serializable::Marshall(defaultAccountDevs).c_str());
         delegate_->SetEqualIdentifier(syncIdentifier, defaultAccountDevs);
     }
 }
