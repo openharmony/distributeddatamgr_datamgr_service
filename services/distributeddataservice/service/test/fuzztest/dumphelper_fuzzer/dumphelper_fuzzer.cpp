37553ee00de1749b27a2e1d9ca5465e3f8aafda2
nwx1285810 <ninghejuan@huawei.com>
2024-05-25 10:14:35 +0000

    feat: add fuzz to service
    Signed-off-by: nwx1285810 <ninghejuan@huawei.com>

---
+++ services/distributeddataservice/service/test/fuzztest/dumphelper_fuzzer/dumphelper_fuzzer.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "dumphelper_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#include "accesstoken_kit.h"
+#include "dump_helper.h"
+#include "ipc_skeleton.h"
+#include "message_parcel.h"
+#include "securec.h"
+#include "token_setproc.h"
+#include <vector>
+
+using namespace OHOS::DistributedData;
+using namespace OHOS::Security::AccessToken;
+
+namespace OHOS {
+
+const std::u16string INTERFACE_TOKEN = u"OHOS.DistributedData.DumpHelper";
+
+bool OnRemoteRequestFuzz(const uint8_t *data, size_t size)
+{
+    int connId = static_cast<int>(*data);
+    std::string deviceId(data, data + size);
+    std::vector<std::string> args = { deviceId + "_1", deviceId + "_2" };
+    DumpHelper::GetInstance().Dump(connId, args);
+
+    return true;
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    if (data == nullptr) {
+        return 0;
+    }
+
+    OHOS::OnRemoteRequestFuzz(data, size);
+
+    return 0;
+}
