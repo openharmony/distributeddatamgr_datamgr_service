faa95a16aaca7686b1aa6564a76f656b4da8e7fe
swx1285818 <suoqilong@huawei.com>
2024-05-31 09:37:39 +0000

    add CloudServer and CloudDB interface case
    Signed-off-by: swx1285818 <suoqilong@huawei.com>

--- services/distributeddataservice/framework/test/cloud_test.cpp
+++ services/distributeddataservice/framework/test/cloud_test.cpp
@@ -17,19 +17,70 @@
 #include <gtest/gtest.h>
 
 #include "serializable/serializable.h"
+#include "cloud/cloud_db.h"
 #include "cloud/cloud_info.h"
+#include "cloud/cloud_server.h"
 #include "cloud/schema_meta.h"
 #include "nlohmann/json.hpp"
 #include "utils/crypto.h"
+#include "store/general_store.h"
+#include "store/general_value.h"
+#include "store/general_watcher.h"
 
 using namespace testing::ext;
 using namespace OHOS::DistributedData;
+namespace OHOS::Test {
 class CloudInfoTest : public testing::Test {
 public:
     static void SetUpTestCase(void){};
     static void TearDownTestCase(void){};
     void SetUp(){};
     void TearDown(){};
+};
+
+class ServicesCloudServerTest : public testing::Test {
+public:
+    static void SetUpTestCase(void){};
+    static void TearDownTestCase(void){};
+    void SetUp(){};
+    void TearDown(){};
+};
+
+class ServicesCloudDBTest : public testing::Test {
+public:
+    static void SetUpTestCase(void){};
+    static void TearDownTestCase(void){};
+    void SetUp(){};
+    void TearDown(){};
+};
+
+class MockGeneralWatcher : public DistributedData::GeneralWatcher {
+public:
+    int32_t OnChange(const Origin &origin, const PRIFields &primaryFields,
+        ChangeInfo &&values) override
+    {
+        return GeneralError::E_OK;
+    }
+
+    int32_t OnChange(const Origin &origin, const Fields &fields, ChangeData &&datas) override
+    {
+        return GeneralError::E_OK;
+    }
+};
+
+class MockQuery : public GenQuery {
+public:
+    static const uint64_t TYPE_ID = 1;
+
+    bool IsEqual(uint64_t tid) override
+    {
+        return tid == TYPE_ID;
+    }
+
+    std::vector<std::string> GetTables() override
+    {
+        return {"table1", "table2"};
+    }
 };
 
 /**
@@ -336,3 +387,105 @@ HWTEST_F(CloudInfoTest, GetSchemaKeyTest4, TestSize.Level0)
     auto result = cloudInfo.GetSchemaKey();
     ASSERT_EQ(info, result);
 }
+
+/**
+* @tc.name: CloudServer
+* @tc.desc: test CloudServer function.
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author: SQL
+*/
+HWTEST_F(ServicesCloudServerTest, CloudServer, TestSize.Level0)
+{
+    CloudServer cloudServer;
+    int32_t userId = 100;
+    cloudServer.GetServerInfo(userId);
+    std::string bundleName = "com.ohos.cloudtest";
+    cloudServer.GetAppSchema(userId, bundleName);
+    std::map<std::string, std::vector<CloudServer::Database>> dbs;
+    auto result1 = cloudServer.Subscribe(userId, dbs);
+    EXPECT_EQ(result1, GeneralError::E_OK);
+    result1 = cloudServer.Unsubscribe(userId, dbs);
+    EXPECT_EQ(result1, GeneralError::E_OK);
+
+    int user = 1;
+    uint32_t tokenId = 123;
+    CloudServer::Database dbMeta;
+    auto result2 = cloudServer.ConnectAssetLoader(tokenId, dbMeta);
+    EXPECT_EQ(result2, nullptr);
+    result2 = cloudServer.ConnectAssetLoader(bundleName, user, dbMeta);
+    EXPECT_EQ(result2, nullptr);
+
+    auto result3 = cloudServer.ConnectCloudDB(tokenId, dbMeta);
+    EXPECT_EQ(result3, nullptr);
+    result3 = cloudServer.ConnectCloudDB(bundleName, user, dbMeta);
+    EXPECT_EQ(result3, nullptr);
+
+    auto result4 = cloudServer.ConnectSharingCenter(userId, bundleName);
+    EXPECT_EQ(result4, nullptr);
+    cloudServer.Clean(userId);
+    cloudServer.ReleaseUserInfo(userId);
+    std::shared_ptr<ExecutorPool> executor = std::make_shared<ExecutorPool>(1, 0);
+    cloudServer.Bind(executor);
+    auto result5 = cloudServer.IsSupportCloud(userId);
+    EXPECT_FALSE(result5);
+}
+
+/**
+* @tc.name: CloudDB
+* @tc.desc: test CloudDB function.
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author: SQL
+*/
+HWTEST_F(ServicesCloudDBTest, CloudDB, TestSize.Level0)
+{
+    CloudDB cloudDB;
+    std::string table = "table";
+    VBucket extend;
+    VBuckets values;
+    auto result1 = cloudDB.Execute(table, "sql", extend);
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+    result1 = cloudDB.BatchInsert(table, std::move(values), values);
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+    result1 = cloudDB.BatchUpdate(table, std::move(values), values);
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+    result1 = cloudDB.BatchDelete(table, values);
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+    result1 = cloudDB.PreSharing(table, values);
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+
+    CloudDB::Devices devices;
+    MockQuery query;
+    CloudDB::Async async;
+    result1 = cloudDB.Sync(devices, 0, query, async, 0);
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+
+    MockGeneralWatcher watcher;
+    result1 = cloudDB.Watch(1, watcher);
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+    result1 = cloudDB.Unwatch(1, watcher);
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+
+    result1 = cloudDB.Lock();
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+    result1 = cloudDB.Unlock();
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+    result1 = cloudDB.Heartbeat();
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+    result1 = cloudDB.Close();
+    EXPECT_EQ(result1, GeneralError::E_NOT_SUPPORT);
+
+    auto result2 = cloudDB.Query(table, extend);
+    EXPECT_EQ(result2, nullptr);
+    result2 = cloudDB.Query(query, extend);
+    EXPECT_EQ(result2, nullptr);
+
+    auto result3 = cloudDB.AliveTime();
+    EXPECT_EQ(result3, -1);
+    std::string bundleName = "com.ohos.cloudDBtest";
+    auto result4 = cloudDB.GetEmptyCursor(bundleName);
+    std::pair<int32_t, std::string> cursor = { GeneralError::E_NOT_SUPPORT, "" };
+    EXPECT_EQ(result4, cursor);
+}
+} // namespace OHOS::Test
