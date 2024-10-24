7fa7b436929578933083569a2592174b7d5f568f
wTong6 <wangtong94@huawei.com>
2024-10-24 11:51:43 +0800

    fix:batch download asset
    Signed-off-by: wTong6 <wangtong94@huawei.com>

--- services/distributeddataservice/service/rdb/rdb_asset_loader.cpp
+++ services/distributeddataservice/service/rdb/rdb_asset_loader.cpp
@@ -16,9 +16,12 @@
 #define LOG_TAG "RdbAssetLoader"
 #include "rdb_asset_loader.h"
 
+#include <variant>
+
 #include "error/general_error.h"
 #include "log_print.h"
 #include "rdb_cloud.h"
+#include "store/general_value.h"
 #include "value_proxy.h"
 
 using namespace DistributedDB;
@@ -44,11 +47,94 @@ DBStatus RdbAssetLoader::Download(const std::string &tableName, const std::strin
                               : CLOUD_RECORD_EXIST_CONFLICT;
 }
 
+void RdbAssetLoader::BatchDownload(const std::string &tableName, std::vector<AssetRecord> &downloadAssets)
+{
+    std::vector<AssetsRecord> assetsRecords = Convert(std::move(downloadAssets));
+    std::set<std::string> skipAssets;
+    std::set<std::string> deleteAssets;
+    PostEvent(skipAssets, assetsRecords, DistributedData::AssetEvent::DOWNLOAD, deleteAssets);
+    assetLoader_->Download(tableName, assetsRecords);
+    PostEvent(skipAssets, assetsRecords, DistributedData::AssetEvent::DOWNLOAD_FINISHED, deleteAssets);
+    downloadAssets = Convert(std::move(assetsRecords));
+}
+
+std::vector<RdbAssetLoader::AssetsRecord> RdbAssetLoader::Convert(std::vector<AssetRecord> &&downloadAssets)
+{
+    std::vector<AssetsRecord> assetsRecords;
+    for (auto &assetRecord : downloadAssets) {
+        AssetsRecord assetsRecord;
+        assetsRecord.gid = std::move(assetRecord.gid);
+        DistributedDB::Type prefixTemp = assetRecord.prefix;
+        assetsRecord.prefix = ValueProxy::Convert(std::move(prefixTemp));
+        assetsRecord.assets = ValueProxy::Convert(std::move(assetRecord.assets));
+        assetsRecords.emplace_back(std::move(assetsRecord));
+    }
+    return assetsRecords;
+}
+
+void RdbAssetLoader::UpdateStatus(AssetRecord &assetRecord, const VBucket &assets)
+{
+    for (const auto &[key, value] : assets) {
+        auto downloadAssets = std::get_if<DistributedData::Assets>(&value);
+        if (downloadAssets == nullptr) {
+            continue;
+        }
+        for (const auto &asset : *downloadAssets) {
+            if (assetRecord.status == DBStatus::OK) {
+                assetRecord.status = ConvertStatus(static_cast<AssetStatus>(asset.status));
+                return;
+            }
+        }
+    }
+}
+
+std::vector<IAssetLoader::AssetRecord> RdbAssetLoader::Convert(std::vector<AssetsRecord> &&assetsRecords)
+{
+    std::vector<AssetRecord> assetRecords;
+    for (auto &assetsRecord : assetsRecords) {
+        AssetRecord assetRecord{
+            .gid = std::move(assetsRecord.gid),
+        };
+        UpdateStatus(assetRecord, assetsRecord.assets);
+        assetRecord.assets = ValueProxy::Convert(std::move(assetsRecord.assets));
+        assetRecords.emplace_back(std::move(assetRecord));
+    }
+    return assetRecords;
+}
+
+DBStatus RdbAssetLoader::ConvertStatus(AssetStatus error)
+{
+    switch (error) {
+        case AssetStatus::STATUS_NORMAL:
+            return DBStatus::OK;
+        case AssetStatus::STATUS_DOWNLOADING:
+            return DBStatus::OK;
+        default:
+            ZLOGE("error:0x%{public}x", error);
+            break;
+    }
+    return DBStatus::CLOUD_ERROR;
+}
+
 DBStatus RdbAssetLoader::RemoveLocalAssets(const std::vector<Asset> &assets)
 {
     DistributedData::VBucket deleteAssets = ValueProxy::Convert(std::map<std::string, Assets>{{ "", assets }});
     auto error = assetLoader_->RemoveLocalAssets("", "", {}, deleteAssets);
     return RdbCloud::ConvertStatus(static_cast<DistributedData::GeneralError>(error));
+}
+
+void RdbAssetLoader::PostEvent(std::set<std::string> &skipAssets, std::vector<AssetsRecord> &assetsRecords,
+    DistributedData::AssetEvent eventId, std::set<std::string> &deleteAssets)
+{
+    for (auto &assetsRecord : assetsRecords) {
+        for (auto &asset : assetsRecord.assets) {
+            auto *downLoadAssets = Traits::get_if<DistributedData::Assets>(&asset.second);
+            if (downLoadAssets == nullptr) {
+                return;
+            }
+            PostEvent(eventId, *downLoadAssets, skipAssets, deleteAssets);
+        }
+    }
 }
 
 void RdbAssetLoader::PostEvent(std::set<std::string>& skipAssets, std::map<std::string, DistributedData::Value>& assets,
--- services/distributeddataservice/service/rdb/rdb_asset_loader.h
+++ services/distributeddataservice/service/rdb/rdb_asset_loader.h
@@ -29,23 +29,37 @@ public:
     using Asset = DistributedDB::Asset;
     using DBStatus = DistributedDB::DBStatus;
     using BindAssets = DistributedData::BindAssets;
+    using AssetsRecord = DistributedData::AssetRecord;
+    using AssetStatus = DistributedData::Asset::Status;
+    using GeneralError = DistributedData::GeneralError;
+    using VBucket = DistributedData::VBucket;
 
-    explicit RdbAssetLoader(std::shared_ptr<DistributedData::AssetLoader> cloudAssetLoader, BindAssets* bindAssets);
+    explicit RdbAssetLoader(std::shared_ptr<DistributedData::AssetLoader> cloudAssetLoader, BindAssets *bindAssets);
 
     ~RdbAssetLoader() = default;
 
     DBStatus Download(const std::string &tableName, const std::string &gid, const Type &prefix,
         std::map<std::string, std::vector<Asset>> &assets) override;
 
+    void BatchDownload(const std::string &tableName, std::vector<AssetRecord> &downloadAssets) override;
+
     DBStatus RemoveLocalAssets(const std::vector<Asset> &assets) override;
 
 private:
+    static std::vector<AssetRecord> Convert(std::vector<AssetsRecord> &&assetsRecords);
+    static std::vector<AssetsRecord> Convert(std::vector<AssetRecord> &&assetRecords);
+    static DBStatus ConvertStatus(AssetStatus error);
+    static void UpdateStatus(AssetRecord &assetRecord, const VBucket &assets);
+
+    void PostEvent(std::set<std::string> &skipAssets, std::map<std::string, DistributedData::Value> &assets,
+        DistributedData::AssetEvent eventId, std::set<std::string> &deleteAssets);
+    void PostEvent(std::set<std::string> &skipAssets, std::vector<AssetsRecord> &assetsRecords,
+        DistributedData::AssetEvent eventId, std::set<std::string> &deleteAssets);
+    void PostEvent(DistributedData::AssetEvent eventId, DistributedData::Assets &assets,
+        std::set<std::string> &skipAssets, std::set<std::string> &deleteAssets);
+
     std::shared_ptr<DistributedData::AssetLoader> assetLoader_;
-    BindAssets* snapshots_;
-    void PostEvent(std::set<std::string>& skipAssets, std::map<std::string, DistributedData::Value>& assets,
-        DistributedData::AssetEvent eventId, std::set<std::string>& deleteAssets);
-    void PostEvent(DistributedData::AssetEvent eventId, DistributedData::Assets& assets,
-        std::set<std::string>& skipAssets, std::set<std::string>& deleteAssets);
+    BindAssets *snapshots_;
 };
 } // namespace OHOS::DistributedRdb
 #endif // OHOS_DISTRIBUTED_DATA_DATAMGR_SERVICE_RDB_ASSET_LOADER_H
