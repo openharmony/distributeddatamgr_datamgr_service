2262ddee4097f5e52e377d8b70cf1e543ca17533
yangliu <yangliu178@huawei.com>
2024-11-18 22:01:22 +0800

    update
    Signed-off-by: yangliu <yangliu178@huawei.com>

--- services/distributeddataservice/service/test/kvdb_service_impl_test.cpp
+++ services/distributeddataservice/service/test/kvdb_service_impl_test.cpp
@@ -598,8 +598,9 @@ HWTEST_F(KvdbServiceImplTest, ResolveAutoLaunch, TestSize.Level0)
     EXPECT_NE(kvStore, nullptr);
     EXPECT_EQ(status, Status::SUCCESS);
     std::string identifier = "identifier";
+    bool isFind = false;
     DistributedKv::KVDBServiceImpl::DBLaunchParam launchParam;
-    auto result = kvdbServiceImpl_->ResolveAutoLaunch(identifier, launchParam);
+    auto result = kvdbServiceImpl_->ResolveAutoLaunch(identifier, launchParam, isFind);
     EXPECT_EQ(result, Status::STORE_NOT_FOUND);
     std::shared_ptr<ExecutorPool> executors = std::make_shared<ExecutorPool>(1, 0);
     Bootstrap::GetInstance().LoadDirectory();
@@ -607,7 +608,7 @@ HWTEST_F(KvdbServiceImplTest, ResolveAutoLaunch, TestSize.Level0)
     DistributedKv::KvStoreMetaManager::GetInstance().BindExecutor(executors);
     DistributedKv::KvStoreMetaManager::GetInstance().InitMetaParameter();
     DistributedKv::KvStoreMetaManager::GetInstance().InitMetaListener();
-    result = kvdbServiceImpl_->ResolveAutoLaunch(identifier, launchParam);
+    result = kvdbServiceImpl_->ResolveAutoLaunch(identifier, launchParam, isFind);
     EXPECT_EQ(result, Status::SUCCESS);
 }
 
