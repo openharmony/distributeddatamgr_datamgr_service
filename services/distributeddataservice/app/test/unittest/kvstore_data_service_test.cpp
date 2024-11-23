ad9f5714172af788143d5e6eeaef96e9f1999fd9
suoqilong <suoqilong@huawei.com>
2024-11-23 10:30:47 +0000

    add UpgradeManagerTest ut case
    Signed-off-by: suoqilong <suoqilong@huawei.com>

--- services/distributeddataservice/app/test/unittest/kvstore_data_service_test.cpp
+++ services/distributeddataservice/app/test/unittest/kvstore_data_service_test.cpp
@@ -13,22 +13,26 @@
  * limitations under the License.
  */
 
+#include "auth_delegate.h"
+#include "bootstrap.h"
 #include "executor_pool.h"
 #include <memory>
+#include "metadata/store_meta_data.h"
+#include <nlohmann/json.hpp>
 #include "kvstore_client_death_observer.h"
 #include "bootstrap.h"
 #include "gtest/gtest.h"
 #include "kvstore_data_service.h"
+#include "serializable/serializable.h"
 #include "system_ability.h"
 #include "system_ability_definition.h"
 #include "upgrade_manager.h"
-
 
 using namespace testing::ext;
 using namespace OHOS::DistributedKv;
 using namespace OHOS::DistributedData;
 using namespace OHOS;
-
+using StoreMetaData = DistributedData::StoreMetaData;
 namespace OHOS::Test {
 class KvStoreDataServiceTest : public testing::Test {
 public:
@@ -211,7 +215,7 @@ HWTEST_F(KvStoreDataServiceTest, AppExit001, TestSize.Level1)
 * @tc.desc:
 * @tc.type: FUNC
 * @tc.require:
-* @tc.author: sql
+* @tc.author: SQL
 */
 HWTEST_F(KvStoreDataServiceTest, OnRemoveSystemAbility001, TestSize.Level1)
 {
@@ -222,6 +226,176 @@ HWTEST_F(KvStoreDataServiceTest, OnRemoveSystemAbility001, TestSize.Level1)
 
     systemAbilityId = COMMON_EVENT_SERVICE_ID;
     EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.OnRemoveSystemAbility(systemAbilityId, deviceId));
+}
+
+/**
+* @tc.name: OnStoreMetaChanged001
+* @tc.desc:
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author: SQL
+*/
+HWTEST_F(KvStoreDataServiceTest, OnStoreMetaChanged001, TestSize.Level1)
+{
+    KvStoreDataService kvStoreDataServiceTest;
+    std::vector<uint8_t> key = { 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01};
+    std::vector<uint8_t> value = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    CHANGE_FLAG flag = CHANGE_FLAG::UPDATE;
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.OnStoreMetaChanged(key, value, flag));
+}
+
+/**
+* @tc.name: AccountEventChanged001
+* @tc.desc:
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author: SQL
+*/
+HWTEST_F(KvStoreDataServiceTest, AccountEventChanged001, TestSize.Level1)
+{
+    KvStoreDataService kvStoreDataServiceTest;
+    AccountEventInfo eventInfo;
+    eventInfo.status = AccountStatus::DEVICE_ACCOUNT_DELETE;
+    eventInfo.userId = "100";
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.AccountEventChanged(eventInfo));
+
+    eventInfo.userId = "";
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.AccountEventChanged(eventInfo));
+
+    eventInfo.status = AccountStatus::DEVICE_ACCOUNT_SWITCHED;
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.AccountEventChanged(eventInfo));
+
+    eventInfo.status = AccountStatus::DEVICE_ACCOUNT_UNLOCKED;
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.AccountEventChanged(eventInfo));
+}
+
+/**
+* @tc.name: InitSecurityAdapter001
+* @tc.desc:
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author: SQL
+*/
+HWTEST_F(KvStoreDataServiceTest, InitSecurityAdapter001, TestSize.Level1)
+{
+    KvStoreDataService kvStoreDataServiceTest;
+    std::shared_ptr<ExecutorPool> executors2 = std::make_shared<ExecutorPool>(1, 0);
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.InitSecurityAdapter(executors2));
+}
+
+/**
+* @tc.name: OnDeviceOnline001
+* @tc.desc:
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author: SQL
+*/
+HWTEST_F(KvStoreDataServiceTest, OnDeviceOnline001, TestSize.Level1)
+{
+    KvStoreDataService kvStoreDataServiceTest;
+    AppDistributedKv::DeviceInfo info1;
+    info1.uuid = "";
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.OnDeviceOnline(info1));
+
+    AppDistributedKv::DeviceInfo info;
+    info.uuid = "ohos.test.uuid";
+    info.udid = "ohos.test.udid";
+    info.networkId = "ohos.test.networkId";
+    info.deviceName = "ohos.test.deviceName";
+    info.deviceType = 1;
+    info.osType = 1;
+    info.authForm = 1;
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.OnDeviceOnline(info));
+}
+
+/**
+* @tc.name: OnDeviceOffline001
+* @tc.desc:
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author: SQL
+*/
+HWTEST_F(KvStoreDataServiceTest, OnDeviceOffline001, TestSize.Level1)
+{
+    KvStoreDataService kvStoreDataServiceTest;
+    AppDistributedKv::DeviceInfo info1;
+    info1.uuid = "";
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.OnDeviceOffline(info1));
+
+    AppDistributedKv::DeviceInfo info;
+    info.uuid = "ohos.test.uuid";
+    info.udid = "ohos.test.udid";
+    info.networkId = "ohos.test.networkId";
+    info.deviceName = "ohos.test.deviceName";
+    info.deviceType = 1;
+    info.osType = 1;
+    info.authForm = 1;
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.OnDeviceOffline(info));
+}
+
+/**
+* @tc.name: OnDeviceOnReady001
+* @tc.desc:
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author: SQL
+*/
+HWTEST_F(KvStoreDataServiceTest, OnDeviceOnReady001, TestSize.Level1)
+{
+    KvStoreDataService kvStoreDataServiceTest;
+    AppDistributedKv::DeviceInfo info1;
+    info1.uuid = "";
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.OnDeviceOnReady(info1));
+
+    AppDistributedKv::DeviceInfo info;
+    info.uuid = "ohos.test.uuid";
+    info.udid = "ohos.test.udid";
+    info.networkId = "ohos.test.networkId";
+    info.deviceName = "ohos.test.deviceName";
+    info.deviceType = 1;
+    info.osType = 1;
+    info.authForm = 1;
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.OnDeviceOnReady(info));
+}
+
+/**
+* @tc.name: OnSessionReady001
+* @tc.desc:
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author: SQL
+*/
+HWTEST_F(KvStoreDataServiceTest, OnSessionReady001, TestSize.Level1)
+{
+    KvStoreDataService kvStoreDataServiceTest;
+    AppDistributedKv::DeviceInfo info1;
+    info1.uuid = "";
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.OnSessionReady(info1));
+
+    AppDistributedKv::DeviceInfo info;
+    info.uuid = "ohos.test.uuid";
+    info.udid = "ohos.test.udid";
+    info.networkId = "ohos.test.networkId";
+    info.deviceName = "ohos.test.deviceName";
+    info.deviceType = 1;
+    info.osType = 1;
+    info.authForm = 1;
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.OnSessionReady(info));
+}
+
+/**
+* @tc.name: DumpStoreInfo001
+* @tc.desc:
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author: SQL
+*/
+HWTEST_F(KvStoreDataServiceTest, DumpStoreInfo001, TestSize.Level1)
+{
+    KvStoreDataService kvStoreDataServiceTest;
+    int fd = 0;
+    std::map<std::string, std::vector<std::string>> params = {};
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.DumpStoreInfo(fd, params));
 }
 
 /**
@@ -236,8 +410,9 @@ HWTEST_F(UpgradeManagerTest, UpgradeManagerTest001, TestSize.Level0)
     auto executors = std::make_shared<ExecutorPool>(1, 0);
     DistributedData::UpgradeManager instance;
     instance.Init(executors);
-
     EXPECT_TRUE(instance.executors_);
+
     instance.Init(executors);
+    EXPECT_TRUE(instance.executors_);
 }
 } // namespace OHOS::Test
