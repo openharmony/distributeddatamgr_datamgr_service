beeb02604556832681018e0b48c00ac4accfd63f
wenjinchao <wenjinchao@huawei.com>
2025-01-22 17:42:07 +0800

    final
    Signed-off-by: wenjinchao <wenjinchao@huawei.com>
    Change-Id: I720445ee552db0c03a4ca00cfdf85451a43cf45c

--- services/distributeddataservice/service/udmf/preprocess/data_handler.cpp
+++ services/distributeddataservice/service/udmf/preprocess/data_handler.cpp
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "error_code.h"
 #define LOG_TAG "DataHandler"
 
 #include "data_handler.h"
