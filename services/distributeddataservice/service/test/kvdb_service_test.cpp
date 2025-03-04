0488abcb42276ccde15116ad5124518c108dfcdc
suoqilong <suoqilong@huawei.com>
2025-03-04 09:10:02 +0000

    add rate of coverage ut case-0303
    Signed-off-by: suoqilong <suoqilong@huawei.com>

--- services/distributeddataservice/service/test/kvdb_service_test.cpp
+++ services/distributeddataservice/service/test/kvdb_service_test.cpp
@@ -123,21 +123,6 @@ public:
         OHOS::DistributedKv::IKvStoreObserver::Keys &&keys) override;
 };
 
-IKvStoreObserverMock::IKvStoreObserverMock(const sptr<IRemoteObject> &impl) : IRemoteProxy<IKvStoreObserver>(impl)
-{
-}
-
-void IKvStoreObserverMock::OnChange(const OHOS::DistributedKv::ChangeNotification &changeNotification)
-{
-    return;
-}
-
-void IKvStoreObserverMock::OnChange(const OHOS::DistributedKv::DataOrigin &origin,
-    OHOS::DistributedKv::IKvStoreObserver::Keys &&keys)
-{
-    return;
-}
-
 class KVDBWatcherTest : public testing::Test {
 public:
     static void SetUpTestCase(void){};
@@ -410,13 +395,6 @@ HWTEST_F(KVDBWatcherTest, OnChange001, TestSize.Level0)
     EXPECT_EQ(watcher->observer_, nullptr);
     auto result = watcher->OnChange(origin, primaryFields, std::move(values));
     EXPECT_EQ(result, GeneralError::E_NOT_INIT);
-
-    sptr<IRemoteObject> impl_;
-    sptr<OHOS::DistributedKv::IKvStoreObserver> observers = new (std::nothrow) IKvStoreObserverMock(impl_);
-    watcher->SetObserver(observers);
-    EXPECT_NE(watcher->observer_, nullptr);
-    auto results = watcher->OnChange(origin, primaryFields, std::move(values));
-    EXPECT_EQ(results, GeneralError::E_OK);
 }
 
 /**
