d9eccbdb9b48405a84801dec2be1a2cc6f73426a
bjd <baijidong@huawei.com>
2025-03-11 21:34:55 +0800

    BugFix
    Signed-off-by: bjd <baijidong@huawei.com>

--- services/distributeddataservice/service/kvdb/kvdb_general_store.cpp
+++ services/distributeddataservice/service/kvdb/kvdb_general_store.cpp
@@ -634,6 +634,14 @@ void KVDBGeneralStore::ObserverProxy::OnChange(DBOrigin origin, const std::strin
         auto &info = changeInfo[storeId_][i];
         for (auto &priData : data.primaryData[i]) {
             Watcher::PRIValue value;
+            if (priData.empty()) {
+                ZLOGW("priData is empty, store:%{public}s table:%{public}s data change "
+                "from :%{public}s, i=%{public}s",
+                Anonymous::Change(storeId_).c_str(),
+                Anonymous::Change(data.tableName).c_str(),
+                Anonymous::Change(originalId).c_str(), i);
+                continue;
+            }
             Convert(std::move(*(priData.begin())), value);
             info.push_back(std::move(value));
         }
