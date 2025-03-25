8b7139d3f555edf54ffc9289b87b3447a4a7e316
z30053452 <zhiyihang1@huawei.com>
2025-03-25 20:20:49 +0800

    code fix
    Signed-off-by: z30053452 <zhiyihang1@huawei.com>

--- services/distributeddataservice/service/test/kvdb_service_impl_test.cpp
+++ services/distributeddataservice/service/test/kvdb_service_impl_test.cpp
@@ -1074,16 +1074,16 @@ HWTEST_F(KvdbServiceImplTest, ResolveAutoLaunch, TestSize.Level0)
 }
 
 /**
-* @tc.name: IsRemoteChange
-* @tc.desc: IsRemoteChange function test.
+* @tc.name: IsNeedSync
+* @tc.desc: IsNeedSync function test.
 * @tc.type: FUNC
 * @tc.author: wangbin
 */
-HWTEST_F(KvdbServiceImplTest, IsRemoteChangeTest, TestSize.Level0)
+HWTEST_F(KvdbServiceImplTest, IsNeedSyncTest, TestSize.Level0)
 {
     StoreMetaData meta = kvdbServiceImpl_->GetStoreMetaData(appId, storeId);
-    std::string devices= "IsRemoteChangeTest";
-    auto changes = kvdbServiceImpl_->IsRemoteChange(meta, devices);
+    std::vector<std::string> devices= {"IsNeedSyncTest"};
+    auto changes = kvdbServiceImpl_->IsNeedSync(meta, devices);
     EXPECT_EQ(changes, true);
 }
 
