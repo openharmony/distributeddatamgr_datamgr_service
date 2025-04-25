9d109a8b6c1dda4699bb967240a5139723d3270f
guochao <guochao76@huawei.com>
2025-04-25 17:09:06 +0800

    add test Signed-off-by: guochao <guochao76@huawei.com>

--- services/distributeddataservice/app/test/unittest/session_manager_test.cpp
+++ services/distributeddataservice/app/test/unittest/session_manager_test.cpp
@@ -13,13 +13,15 @@
  * limitations under the License.
  */
 
+#include "session_manager/session_manager.h"
+
 #include "accesstoken_kit.h"
 #include "account_delegate_mock.h"
+#include "auth_delegate_mock.h"
 #include "bootstrap.h"
 #include "device_manager_adapter.h"
 #include "device_manager_adapter_mock.h"
 #include "gtest/gtest.h"
-#include "kvstore_meta_manager.h"
 #include "meta_data_manager_mock.h"
 #include "metadata/meta_data_manager.h"
 #include "metadata/store_meta_data.h"
@@ -70,15 +72,50 @@ void GrantPermissionNative()
 
 class SessionManagerTest : public testing::Test {
 public:
+    void CreateUserStatus(std::vector<UserStatus> &users)
+    {
+        UserStatus stat;
+        stat.id = 0;
+        UserStatus stat1;
+        stat.id = 1;
+        UserStatus stat2;
+        stat.id = 2;
+        UserStatus stat3;
+        stat.id = 3;
+        users.push_back(stat);
+        users.push_back(stat1);
+        users.push_back(stat2);
+        users.push_back(stat3);
+    }
+    void CreateStoreMetaData(std::vector<StoreMetaData> &datas, SessionPoint local)
+    {
+        StoreMetaData data;
+        data.appId = local.appId;
+        data.storeId = local.storeId;
+        data.bundleName = "com.test.session";
+        StoreMetaData data1;
+        data1.appId = local.appId;
+        data1.storeId = "local.storeId";
+        data1.bundleName = "com.test.session1";
+        StoreMetaData data2;
+        data2.appId = "local.appId";
+        data2.storeId = local.storeId;
+        data2.bundleName = "com.test.session2";
+        StoreMetaData data3;
+        data3.appId = "local.appId";
+        data3.storeId = "local.storeId";
+        data3.bundleName = "com.test.session3";
+        datas.push_back(data);
+        datas.push_back(data1);
+        datas.push_back(data2);
+        datas.push_back(data3);
+    }
     static void SetUpTestCase()
     {
         auto executors = std::make_shared<ExecutorPool>(12, 5);
         Bootstrap::GetInstance().LoadComponents();
         Bootstrap::GetInstance().LoadDirectory();
         Bootstrap::GetInstance().LoadCheckers();
-        KvStoreMetaManager::GetInstance().BindExecutor(executors);
-        KvStoreMetaManager::GetInstance().InitMetaParameter();
-        KvStoreMetaManager::GetInstance().InitMetaListener();
         DeviceManagerAdapter::GetInstance().Init(executors);
 
         // init peer device
@@ -116,6 +153,8 @@ public:
         BDeviceManagerAdapter::deviceManagerAdapter = deviceManagerAdapterMock;
         metaDataManagerMock = std::make_shared<MetaDataManagerMock>();
         BMetaDataManager::metaDataManager = metaDataManagerMock;
+        metaDataMock = std::make_shared<MetaDataMock<StoreMetaData>>();
+        BMetaData<StoreMetaData>::metaDataManager = metaDataMock;
         userDelegateMock = std::make_shared<UserDelegateMock>();
         BUserDelegate::userDelegate = userDelegateMock;
     }
@@ -139,6 +178,8 @@ public:
         BDeviceManagerAdapter::deviceManagerAdapter = nullptr;
         metaDataManagerMock = nullptr;
         BMetaDataManager::metaDataManager = nullptr;
+        metaDataMock = nullptr;
+        BMetaData<StoreMetaData>::metaDataManager = nullptr;
         userDelegateMock = nullptr;
         BUserDelegate::userDelegate = nullptr;
     }
@@ -152,31 +193,28 @@ public:
     void ConstructValidData()
     {
         constexpr size_t BUFFER_SIZE = sizeof(dataBuffer);
-        memset_s(dataBuffer, BUFFER_SIZE, 0, BUFFER_SIZE); // 清空缓冲区
+        memset_s(dataBuffer, BUFFER_SIZE, 0, BUFFER_SIZE);
 
-        // 构建有效数据
         RouteHead head{};
         head.magic = RouteHead::MAGIC_NUMBER;
         head.version = RouteHead::VERSION;
         head.dataLen = sizeof(SessionDevicePair) + sizeof(SessionUserPair) + sizeof(uint32_t) * 1
-                       + sizeof(SessionAppId) + APP_STR_LEN; // 应用ID长度+内容
-        head.checkSum = 0;                                   // 假设校验和正确
+                       + sizeof(SessionAppId) + APP_STR_LEN;
+        head.checkSum = 0;
 
-        // 序列化头部
         uint8_t *ptr = dataBuffer;
         size_t remaining = BUFFER_SIZE;
 
-        // 1. 写入RouteHead
+        // 1. write RouteHead
         errno_t err = memcpy_s(ptr, remaining, &head, sizeof(RouteHead));
         ASSERT_EQ(err, 0) << "Failed to copy RouteHead";
         ptr += sizeof(RouteHead);
         remaining -= sizeof(RouteHead);
 
-        // 2. 写入SessionDevicePair
+        // 2. write SessionDevicePair
         SessionDevicePair devPair{};
-        // 安全初始化设备ID
         constexpr size_t DEV_ID_SIZE = sizeof(devPair.sourceId);
-        err = memset_s(devPair.sourceId, DEV_ID_SIZE, 'A', DEV_ID_SIZE - 1); // 留1字节给终止符
+        err = memset_s(devPair.sourceId, DEV_ID_SIZE, 'A', DEV_ID_SIZE - 1);
         ASSERT_EQ(err, 0) << "Failed to init sourceId";
         devPair.sourceId[DEV_ID_SIZE - 1] = '\0';
 
@@ -189,24 +227,22 @@ public:
         ptr += sizeof(SessionDevicePair);
         remaining -= sizeof(SessionDevicePair);
 
-        // 3. 写入SessionUserPair
+        // 3. write SessionUserPair
         SessionUserPair userPair{};
-        userPair.sourceUserId = HostToNet(100U); // 注意字节序转换
+        userPair.sourceUserId = HostToNet(100U);
         userPair.targetUserCount = HostToNet(1U);
 
         err = memcpy_s(ptr, remaining, &userPair, sizeof(SessionUserPair));
         ASSERT_EQ(err, 0) << "Failed to copy SessionUserPair";
         ptr += sizeof(SessionUserPair);
         remaining -= sizeof(SessionUserPair);
-
-        // 写入目标用户ID
         uint32_t targetUser = HostToNet(200U);
         err = memcpy_s(ptr, remaining, &targetUser, sizeof(uint32_t));
         ASSERT_EQ(err, 0) << "Failed to copy targetUser";
         ptr += sizeof(uint32_t);
         remaining -= sizeof(uint32_t);
 
-        // 4. 写入SessionAppId
+        // 4. write SessionAppId
         SessionAppId appId{};
         const char *appStr = "test";
 
@@ -217,19 +253,18 @@ public:
         ptr += sizeof(SessionAppId);
         remaining -= sizeof(SessionAppId);
 
-        // 写入应用ID内容
         err = memcpy_s(ptr, remaining, appStr, APP_STR_LEN);
         ASSERT_EQ(err, 0) << "Failed to copy appId data";
         ptr += APP_STR_LEN;
         remaining -= APP_STR_LEN;
     }
-    // 验证总长度
     const size_t validTotalLen = sizeof(RouteHead) + sizeof(SessionDevicePair) + sizeof(SessionUserPair)
                                  + sizeof(uint32_t) * 1 + sizeof(SessionAppId) + APP_STR_LEN;
     uint8_t dataBuffer[1024];
-    static constexpr size_t APP_STR_LEN = 5; // 包括终止符
+    static constexpr size_t APP_STR_LEN = 5;
     static inline std::shared_ptr<DeviceManagerAdapterMock> deviceManagerAdapterMock = nullptr;
     static inline std::shared_ptr<MetaDataManagerMock> metaDataManagerMock = nullptr;
+    static inline std::shared_ptr<MetaDataMock<StoreMetaData>> metaDataMock = nullptr;
     static inline std::shared_ptr<UserDelegateMock> userDelegateMock = nullptr;
 };
 
@@ -273,14 +308,10 @@ HWTEST_F(SessionManagerTest, PackAndUnPack01, TestSize.Level2)
 HWTEST_F(SessionManagerTest, GetHeadDataSize_Test1, TestSize.Level1)
 {
     ExtendInfo info;
-    // 创建RouteHeadHandlerImpl实例
     RouteHeadHandlerImpl routeHeadHandlerImpl(info);
     uint32_t headSize = 0;
-    // 设置appId_为Bootstrap::GetInstance().GetProcessLabel()
     routeHeadHandlerImpl.appId_ = Bootstrap::GetInstance().GetProcessLabel();
-    // 调用GetHeadDataSize方法
     auto status = routeHeadHandlerImpl.GetHeadDataSize(headSize);
-    // 验证返回状态和headSize的值
     EXPECT_EQ(status, DistributedDB::OK);
     EXPECT_EQ(headSize, 0);
 }
@@ -293,14 +324,10 @@ HWTEST_F(SessionManagerTest, GetHeadDataSize_Test1, TestSize.Level1)
 HWTEST_F(SessionManagerTest, GetHeadDataSize_Test2, TestSize.Level1)
 {
     ExtendInfo info;
-    // 创建RouteHeadHandlerImpl实例
     RouteHeadHandlerImpl routeHeadHandlerImpl(info);
     uint32_t headSize = 0;
-    // 设置appId_不等于Bootstrap::GetInstance().GetProcessLabel()
     routeHeadHandlerImpl.appId_ = "otherAppId";
-    // 调用GetHeadDataSize方法
     auto status = routeHeadHandlerImpl.GetHeadDataSize(headSize);
-    // 验证返回状态和headSize的值
     EXPECT_EQ(status, DistributedDB::OK);
     EXPECT_EQ(headSize, 0);
 }
@@ -312,20 +339,15 @@ HWTEST_F(SessionManagerTest, GetHeadDataSize_Test2, TestSize.Level1)
   */
 HWTEST_F(SessionManagerTest, GetHeadDataSize_Test3, TestSize.Level1)
 {
-    // 设置devInfo.osType等于OH_OS_TYPE
     DeviceInfo deviceInfo;
     deviceInfo.osType = OH_OS_TYPE;
     EXPECT_CALL(*deviceManagerAdapterMock, GetDeviceInfo(_)).WillRepeatedly(Return(deviceInfo));
     ExtendInfo info;
-    // 创建RouteHeadHandlerImpl实例
     RouteHeadHandlerImpl routeHeadHandlerImpl(info);
     uint32_t headSize = 0;
-    // 设置appId_不等于Bootstrap::GetInstance().GetProcessLabel()
     routeHeadHandlerImpl.appId_ = "otherAppId";
-    // 调用GetHeadDataSize方法
     auto status = routeHeadHandlerImpl.GetHeadDataSize(headSize);
-    // 验证返回状态和headSize的值
-    EXPECT_EQ(status, DistributedDB::OK);
+    EXPECT_EQ(status, DistributedDB::DB_ERROR);
     EXPECT_EQ(headSize, 0);
 }
 /**
@@ -336,20 +358,21 @@ HWTEST_F(SessionManagerTest, GetHeadDataSize_Test3, TestSize.Level1)
   */
 HWTEST_F(SessionManagerTest, GetHeadDataSize_Test4, TestSize.Level1)
 {
+    DeviceInfo deviceInfo;
+    deviceInfo.osType = OH_OS_TYPE;
+    EXPECT_CALL(*deviceManagerAdapterMock, IsOHOSType(_)).WillRepeatedly(Return(true));
+    EXPECT_CALL(*deviceManagerAdapterMock, GetDeviceInfo(_)).WillRepeatedly(Return(deviceInfo));
+
     const DistributedDB::ExtendInfo info = {
         .appId = "otherAppId", .storeId = "test_store", .userId = "100", .dstTarget = PEER_DEVICE_ID
     };
     auto sendHandler = RouteHeadHandlerImpl::Create(info);
     ASSERT_NE(sendHandler, nullptr);
-    // 设置devInfo.osType等于OH_OS_TYPE
-    DeviceInfo deviceInfo;
-    deviceInfo.osType = OH_OS_TYPE;
-    EXPECT_CALL(*deviceManagerAdapterMock, GetDeviceInfo(_)).WillRepeatedly(Return(deviceInfo));
+
     CapMetaData capMetaData;
     capMetaData.version = CapMetaData::CURRENT_VERSION;
     EXPECT_CALL(*metaDataManagerMock, LoadMeta(_, _, _))
         .WillRepeatedly(DoAll(SetArgReferee<1>(capMetaData), Return(true)));
-
     std::vector<UserStatus> userStatus;
     UserStatus userStat1;
     UserStatus userStat2;
@@ -363,9 +386,7 @@ HWTEST_F(SessionManagerTest, GetHeadDataSize_Test4, TestSize.Level1)
     EXPECT_CALL(*userDelegateMock, GetRemoteUserStatus(_)).WillRepeatedly(Return(userStatus));
 
     uint32_t headSize = 0;
-    // 调用GetHeadDataSize方法
     auto status = sendHandler->GetHeadDataSize(headSize);
-    // 验证返回状态和headSize的值
     EXPECT_EQ(status, DistributedDB::OK);
     EXPECT_EQ(headSize, 0);
 
@@ -382,22 +403,19 @@ HWTEST_F(SessionManagerTest, GetHeadDataSize_Test4, TestSize.Level1)
   */
 HWTEST_F(SessionManagerTest, ParseHeadDataUserTest001, TestSize.Level1)
 {
-    // 创建RouteHeadHandlerImpl实例
+    EXPECT_CALL(*deviceManagerAdapterMock, IsOHOSType(_)).WillRepeatedly(Return(true));
     const DistributedDB::ExtendInfo info = {
         .appId = "otherAppId", .storeId = "test_store", .userId = "100", .dstTarget = PEER_DEVICE_ID
     };
     auto sendHandler = RouteHeadHandlerImpl::Create(info);
     ASSERT_NE(sendHandler, nullptr);
 
-    // 创建测试数据
     uint32_t totalLen = 10;
     std::string label = "testLabel";
     std::vector<UserInfo> userInfos;
 
-    // 调用待测试方法
     bool result = sendHandler->ParseHeadDataUser(nullptr, totalLen, label, userInfos);
 
-    // 验证结果
     EXPECT_FALSE(result);
     EXPECT_EQ(userInfos.size(), 0);
 }
@@ -409,22 +427,19 @@ HWTEST_F(SessionManagerTest, ParseHeadDataUserTest001, TestSize.Level1)
   */
 HWTEST_F(SessionManagerTest, ParseHeadDataUserTest002, TestSize.Level1)
 {
-    // 创建RouteHeadHandlerImpl实例
+    EXPECT_CALL(*deviceManagerAdapterMock, IsOHOSType(_)).WillRepeatedly(Return(true));
     const DistributedDB::ExtendInfo info = {
         .appId = "otherAppId", .storeId = "test_store", .userId = "100", .dstTarget = PEER_DEVICE_ID
     };
     auto sendHandler = RouteHeadHandlerImpl::Create(info);
     ASSERT_NE(sendHandler, nullptr);
 
-    // 创建测试数据
     uint8_t data[10] = { 0 };
     std::string label = "testLabel";
     std::vector<UserInfo> userInfos;
 
-    // 调用待测试方法
     bool result = sendHandler->ParseHeadDataUser(data, sizeof(RouteHead) - 1, label, userInfos);
 
-    // 验证结果
     EXPECT_FALSE(result);
     EXPECT_EQ(userInfos.size(), 0);
 }
@@ -437,14 +452,13 @@ HWTEST_F(SessionManagerTest, ParseHeadDataUserTest002, TestSize.Level1)
   */
 HWTEST_F(SessionManagerTest, ParseHeadDataUserTest003, TestSize.Level1)
 {
-    // 创建RouteHeadHandlerImpl实例
+    EXPECT_CALL(*deviceManagerAdapterMock, IsOHOSType(_)).WillRepeatedly(Return(true));
     const DistributedDB::ExtendInfo info = {
         .appId = "otherAppId", .storeId = "test_store", .userId = "100", .dstTarget = PEER_DEVICE_ID
     };
     auto sendHandler = RouteHeadHandlerImpl::Create(info);
     ASSERT_NE(sendHandler, nullptr);
 
-    // 创建测试数据
     uint8_t data[10] = { 0 };
     std::string label = "testLabel";
     std::vector<UserInfo> userInfos;
@@ -453,10 +467,8 @@ HWTEST_F(SessionManagerTest, ParseHeadDataUserTest003, TestSize.Level1)
     head.version = RouteHead::VERSION;
     head.dataLen = static_cast<uint32_t>(sizeof(data) - sizeof(RouteHead));
 
-    // 调用待测试方法
     bool result = sendHandler->ParseHeadDataUser(data, sizeof(RouteHead), label, userInfos);
 
-    // 验证结果
     EXPECT_FALSE(result);
     EXPECT_EQ(userInfos.size(), 0);
 }
@@ -470,9 +482,8 @@ HWTEST_F(SessionManagerTest, ParseHeadDataUserTest003, TestSize.Level1)
 HWTEST_F(SessionManagerTest, UnPackData_InvalidMagic, TestSize.Level1)
 {
     RouteHead *head = reinterpret_cast<RouteHead *>(dataBuffer);
-    head->magic = 0xFFFF; // Invalid magic
+    head->magic = 0xFFFF;
     ExtendInfo info;
-    // 创建RouteHeadHandlerImpl实例
     RouteHeadHandlerImpl routeHeadHandlerImpl(info);
     uint32_t unpackedSize;
     EXPECT_FALSE(routeHeadHandlerImpl.UnPackData(dataBuffer, validTotalLen, unpackedSize));
@@ -487,9 +498,8 @@ HWTEST_F(SessionManagerTest, UnPackData_InvalidMagic, TestSize.Level1)
 HWTEST_F(SessionManagerTest, UnPackData_VersionMismatch, TestSize.Level1)
 {
     RouteHead *head = reinterpret_cast<RouteHead *>(dataBuffer);
-    head->version = 0x00; // Invalid version
+    head->version = 0x00;
     ExtendInfo info;
-    // 创建RouteHeadHandlerImpl实例
     RouteHeadHandlerImpl routeHeadHandlerImpl(info);
     uint32_t unpackedSize;
     EXPECT_FALSE(routeHeadHandlerImpl.UnPackData(dataBuffer, validTotalLen, unpackedSize));
@@ -504,10 +514,95 @@ HWTEST_F(SessionManagerTest, UnPackData_VersionMismatch, TestSize.Level1)
 HWTEST_F(SessionManagerTest, UnPackData_ValidData, TestSize.Level1)
 {
     ExtendInfo info;
-    // 创建RouteHeadHandlerImpl实例
     RouteHeadHandlerImpl routeHeadHandlerImpl(info);
     uint32_t unpackedSize;
     EXPECT_TRUE(routeHeadHandlerImpl.UnPackData(dataBuffer, validTotalLen, unpackedSize));
     EXPECT_EQ(unpackedSize, validTotalLen);
+}
+
+/**
+  * @tc.name: ShouldAddSystemUserWhenLocalUserIdIsSystem
+  * @tc.desc: test GetSession.
+  * @tc.type: FUNC
+  * @tc.author: guochao
+  */
+HWTEST_F(SessionManagerTest, ShouldAddSystemUserWhenLocalUserIdIsSystem, TestSize.Level1)
+{
+    SessionPoint local;
+    local.userId = UserDelegate::SYSTEM_USER;
+    local.appId = "test_app";
+    local.deviceId = "local_device";
+    local.storeId = "test_store";
+
+    std::vector<UserStatus> users;
+    CreateUserStatus(users);
+    EXPECT_CALL(*userDelegateMock, GetRemoteUserStatus(_)).WillOnce(Return(users));
+    EXPECT_CALL(AuthHandlerMock::GetInstance(), CheckAccess(_, _, _, _))
+        .WillOnce(Return(std::pair(true, true)))
+        .WillOnce(Return(std::pair(true, false)))
+        .WillOnce(Return(std::pair(false, true)))
+        .WillOnce(Return(std::pair(false, false)));
+    std::vector<StoreMetaData> datas;
+    CreateStoreMetaData(datas, local);
+    EXPECT_CALL(*metaDataMock, LoadMeta(_, _, _)).WillRepeatedly(DoAll(SetArgReferee<1>(datas), Return(true)));
+
+    Session session = SessionManager::GetInstance().GetSession(local, "target_device");
+    ASSERT_EQ(2, session.targetUserIds.size());
+    EXPECT_EQ(UserDelegate::SYSTEM_USER, session.targetUserIds[0]);
+}
+
+/**
+  * @tc.name: ShouldReturnEarlyWhenGetSendAuthParamsFails
+  * @tc.desc: test GetSession.
+  * @tc.type: FUNC
+  * @tc.author: guochao
+  */
+HWTEST_F(SessionManagerTest, ShouldReturnEarlyWhenGetSendAuthParamsFails, TestSize.Level1)
+{
+    SessionPoint local;
+    local.userId = 100;
+    local.appId = "test_app";
+    local.deviceId = "local_device";
+    EXPECT_CALL(*userDelegateMock, GetRemoteUserStatus(_)).WillOnce(Return(std::vector<UserStatus>{}));
+    std::vector<StoreMetaData> datas;
+    EXPECT_CALL(*metaDataMock, LoadMeta(_, _, _)).WillRepeatedly(Return(false));
+
+    Session session = SessionManager::GetInstance().GetSession(local, "target_device");
+
+    EXPECT_TRUE(session.targetUserIds.empty());
+}
+
+/**
+  * @tc.name: CheckSession
+  * @tc.desc: test CheckSession.
+  * @tc.type: FUNC
+  * @tc.author: guochao
+  */
+HWTEST_F(SessionManagerTest, CheckSession, TestSize.Level1)
+{
+    SessionPoint localSys;
+    localSys.userId = UserDelegate::SYSTEM_USER;
+    localSys.appId = "test_app";
+    localSys.deviceId = "local_device";
+    localSys.storeId = "test_store";
+    SessionPoint localNormal;
+    localNormal.userId = 100;
+    localNormal.appId = "test_app";
+    localNormal.deviceId = "local_device";
+    localNormal.storeId = "test_store";
+    std::vector<StoreMetaData> datas;
+    CreateStoreMetaData(datas, localSys);
+    EXPECT_CALL(*metaDataMock, LoadMeta(_, _, _))
+        .WillOnce(DoAll(SetArgReferee<1>(datas), Return(false)))
+        .WillRepeatedly(DoAll(SetArgReferee<1>(datas), Return(true)));
+    EXPECT_CALL(AuthHandlerMock::GetInstance(), CheckAccess(_, _, _, _))
+        .WillOnce(Return(std::pair(false, true)))
+        .WillOnce(Return(std::pair(true, false)));
+    bool result = SessionManager::GetInstance().CheckSession(localSys, localNormal);
+    EXPECT_FALSE(result);
+    result = SessionManager::GetInstance().CheckSession(localSys, localNormal);
+    EXPECT_FALSE(result);
+    result = SessionManager::GetInstance().CheckSession(localNormal, localSys);
+    EXPECT_TRUE(result);
 }
 } // namespace
