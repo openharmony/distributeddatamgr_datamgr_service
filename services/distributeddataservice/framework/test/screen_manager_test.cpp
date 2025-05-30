22fb4a3bf996829f8d5818f9f62f8367cb53548c
Jeam_wang <jeam.wangwei@huawei.com>
2025-05-30 19:48:54 +0800

    js接口错误码范围限制 及调用rust中vector内存失败
    Signed-off-by: Jeam_wang <jeam.wangwei@huawei.com>

--- services/distributeddataservice/framework/test/screen_manager_test.cpp
+++ services/distributeddataservice/framework/test/screen_manager_test.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2024 Huawei Device Co., Ltd.
+* Copyright (c) 2025 Huawei Device Co., Ltd.
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
@@ -16,9 +16,6 @@
 #define LOG_TAG "ScreenManagerTest"
 #include <gtest/gtest.h>
 
-
-#include "nlohmann/json.hpp"
-#include "utils/crypto.h"
 #include "screen/screen_manager.h"
 
 using namespace testing::ext;
