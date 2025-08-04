8f9b560a17e9228ee8095000bf31ae89ef59f56b
Jeam_wang <jeam.wangwei@h-partners.com>
2025-08-04 09:40:04 +0800

    cloudservicestubfuzz用例修改
    Signed-off-by: Jeam_wang <jeam.wangwei@h-partners.com>

--- services/distributeddataservice/service/test/fuzztest/cloudservicestub_fuzzer/cloudservicestub_fuzzer.cpp
+++ services/distributeddataservice/service/test/fuzztest/cloudservicestub_fuzzer/cloudservicestub_fuzzer.cpp
@@ -75,9 +75,9 @@ bool OnRemoteRequestFuzz(FuzzedDataProvider &provider)
         std::shared_ptr<ExecutorPool> executor = std::make_shared<ExecutorPool>(NUM_MAX, NUM_MIN);
         cloudServiceImpl->OnBind(
             { "CloudServiceStubFuzz", static_cast<uint32_t>(IPCSkeleton::GetSelfTokenID()), std::move(executor) });
+        AllocAndSetHapToken();
     });
-    
-    AllocAndSetHapToken();
+
     uint32_t code = provider.ConsumeIntegralInRange<uint32_t>(CODE_MIN, CODE_MAX);
     std::vector<uint8_t> remainingData = provider.ConsumeRemainingBytes<uint8_t>();
     MessageParcel request;
