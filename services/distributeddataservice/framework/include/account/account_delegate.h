0bfac3d2f50bd25ddfa1a62a55ed774748f13e6c
gecheng <gecheng14@h-partners.com>
2025-08-13 17:43:40 +0800

    解决黄蓝差异
    Signed-off-by: gecheng <gecheng14@h-partners.com>

--- services/distributeddataservice/framework/include/account/account_delegate.h
+++ services/distributeddataservice/framework/include/account/account_delegate.h
@@ -67,8 +67,8 @@ public:
     API_EXPORT virtual void UnsubscribeAccountEvent() = 0;
     API_EXPORT virtual bool QueryUsers(std::vector<int> &users) = 0;
     API_EXPORT virtual bool QueryForegroundUsers(std::vector<int> &users) = 0;
-    API_EXPORT virtual bool IsLoginAccount() = 0;
     API_EXPORT virtual bool QueryForegroundUserId(int &foregroundUserId) = 0;
+    API_EXPORT virtual bool IsLoginAccount() = 0;
     API_EXPORT virtual bool IsVerified(int userId) = 0;
     API_EXPORT virtual bool RegisterHashFunc(HashFunc hash) = 0;
     API_EXPORT virtual bool IsDeactivating(int userId) = 0;
