750ff8af65577e9834f34ab60f76f15bcf1d718d
baijidong <baijidong@huawei.com>
2025-10-23 10:27:10 +0800

    安全问题修复
    Signed-off-by: baijidong <baijidong@huawei.com>

--- services/distributeddataservice/app/test/unittest/kvstore_data_service_test.cpp
+++ services/distributeddataservice/app/test/unittest/kvstore_data_service_test.cpp
@@ -371,6 +371,8 @@ HWTEST_F(KvStoreDataServiceTest, InitSecurityAdapter001, TestSize.Level0)
     KvStoreDataService kvStoreDataServiceTest;
     std::shared_ptr<ExecutorPool> executors2 = std::make_shared<ExecutorPool>(1, 0);
     EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.InitSecurityAdapter(executors2));
+    std::shared_ptr<ExecutorPool> executors3 = nullptr;
+    EXPECT_NO_FATAL_FAILURE(kvStoreDataServiceTest.InitSecurityAdapter(executors3));
 }
 
 /**
