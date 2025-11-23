ab981c9b83f250444ccb7dbc47d5892a44fd04a9
木子李 <liyi263@h-partners.com>
2025-11-23 16:45:52 +0800

    add testcase
    Signed-off-by: 木子李 <liyi263@h-partners.com>

--- services/distributeddataservice/service/test/rdb_general_store_test.cpp
+++ services/distributeddataservice/service/test/rdb_general_store_test.cpp
@@ -2094,6 +2094,7 @@ HWTEST_F(RdbGeneralStoreTest, StopCloudSync, TestSize.Level1)
     EXPECT_EQ(result, E_ALREADY_CLOSED);
     metaData_.storeId = "mock";
     store_ = std::make_shared<RdbGeneralStore>(metaData_);
+    store_->Init();
     result = store_->StopCloudSync();
     EXPECT_EQ(result, E_OK);
 }
@@ -2108,6 +2109,7 @@ HWTEST_F(RdbGeneralStoreTest, OnSyncTrigger, TestSize.Level1)
     MockGeneralWatcher watcher;
     std::string storeId = "testStoreId";
     int32_t triggerMode = 1;
+    store_->Init();
     auto result = store_->Watch(GeneralWatcher::Origin::ORIGIN_ALL, watcher);
     store_->OnSyncTrigger(storeId, triggerMode);
     EXPECT_EQ(result, GeneralError::E_OK);
@@ -2128,6 +2130,7 @@ HWTEST_F(RdbGeneralStoreTest, SetCloudConflictHandle, TestSize.Level1)
     EXPECT_EQ(result, GeneralError::E_ALREADY_CLOSED);
     metaData_.storeId = "mock";
     store_ = std::make_shared<RdbGeneralStore>(metaData_);
+    store_->Init();
     result = store_->SetCloudConflictHandler(handler);
     EXPECT_EQ(result, E_OK);
     result = store_->SetCloudConflictHandler(handler);
---
+++ services/distributeddataservice/service/test/rdb_service_stub_test.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rdb_service_stub.h"
+
+#include "gtest/gtest.h"
+#include "itypes_util.h"
+#include "rdb_service.impl.h"
+
+using namespace testing::ext;
+using namespace OHOS;
+using namespace OHOS::DistributedRdb;
+using namespace OHOS::DistributedData;
+namespace OHOS::Test {
+namespace DistributedRDBTest {
+class RdbServiceStubTest : public testing::Test {
+public:
+    static void SetUpTestCase(void){};
+    static void TearDownTestCase(void){};
+    void SetUp(){};
+    void TearDown(){};
+};
+
+/**
+* @tc.name: OnStopCloudSync001
+* @tc.desc: test stub OnStopCloudSync Unmarshal fail and succ
+* @tc.type: FUNC
+*/
+HWTEST_F(RdbServiceStubTest, OnStopCloudSync001, TestSize.Level1)
+{
+    auto rdbServiceStub = std::make_shared<RdbServiceImpl>();
+    MessageParcel data;
+    MessageParcel reply;
+    auto ret = rdbServiceStub->OnStopCloudSync(data, reply);
+    EXPECT_EQ(ret, IPC_STUB_INVALID_DATA_ERR);
+    RdbSyncerParam param;
+    ITypesUtil::Marshal(data, param);
+    ret = rdbServiceStub->OnStopCloudSync(data, reply);
+    EXPECT_EQ(ret, RDB_OK);
+}
+} // namespace DistributedRDBTest
+} // namespace OHOS::Test
