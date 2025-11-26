9005bfb3de5c99cd1403dc4e408e7ef563c2c700
邓俊 <dengjun39@huawei.com>
2025-11-26 10:01:24 +0800

    fix:修改change接口
    Signed-off-by: 邓俊 <dengjun39@huawei.com>

--- services/distributeddataservice/framework/utils/anonymous.cpp
+++ services/distributeddataservice/framework/utils/anonymous.cpp
@@ -18,63 +18,51 @@
 namespace OHOS {
 namespace DistributedData {
 
-constexpr int32_t CONTINUOUS_DIGITS_MINI_SIZE = 6;
+constexpr int32_t HEAD_SIZE = 3;
+constexpr int32_t END_SIZE = 3;
+constexpr int32_t MIN_SIZE = HEAD_SIZE + END_SIZE + 3;
+constexpr const char *REPLACE_CHAIN = "***";
+constexpr int32_t FILE_PATH_MINI_SIZE = 6;
+constexpr int32_t AREA_MINI_SIZE = 4;
+constexpr int32_t AREA_OFFSET_SIZE = 5;
+constexpr int32_t PRE_OFFSET_SIZE = 1;
 
-std::string AnonyDigits(const std::string &fileName)
+std::string AnonymousName(const std::string &fileName)
 {
-    std::string::size_type digitsNum = fileName.size();
-    if (digitsNum < CONTINUOUS_DIGITS_MINI_SIZE) {
-        return fileName;
-    }
-    std::string::size_type endDigitsNum = 4;
-    std::string::size_type shortEndDigitsNum = 3;
-    std::string name = fileName;
-    std::string last = "";
-    if (digitsNum == CONTINUOUS_DIGITS_MINI_SIZE) {
-        last = name.substr(name.size() - shortEndDigitsNum);
-    } else {
-        last = name.substr(name.size() - endDigitsNum);
+    if (fileName.empty()) {
+        return "";
     }
 
-    return "***" + last;
+    if (fileName.length() <= HEAD_SIZE) {
+        return fileName.substr(0, 1) + "**";
+    }
+
+    if (fileName.length() < MIN_SIZE) {
+        return (fileName.substr(0, HEAD_SIZE) + REPLACE_CHAIN);
+    }
+
+    return (fileName.substr(0, HEAD_SIZE) + REPLACE_CHAIN + fileName.substr(fileName.length() - END_SIZE, END_SIZE));
 }
 
 std::string Anonymous::Change(const std::string &name)
 {
-    std::vector<std::string> alnum;
-    std::vector<std::string> noAlnum;
-    std::string alnumStr;
-    std::string noAlnumStr;
-    for (const auto &letter : name) {
-        if (isxdigit(letter)) {
-            if (!noAlnumStr.empty()) {
-                noAlnum.push_back(noAlnumStr);
-                noAlnumStr.clear();
-                alnum.push_back("");
-            }
-            alnumStr += letter;
-        } else {
-            if (!alnumStr.empty()) {
-                alnum.push_back(alnumStr);
-                alnumStr.clear();
-                noAlnum.push_back("");
-            }
-            noAlnumStr += letter;
-        }
+    auto pre = name.find("/");
+    auto end = name.rfind("/");
+    if (pre == std::string::npos || end - pre < FILE_PATH_MINI_SIZE) {
+        return AnonymousName(name);
     }
-    if (!alnumStr.empty()) {
-        alnum.push_back(alnumStr);
-        noAlnum.push_back("");
+    auto path = name.substr(pre, end - pre);
+    auto area = path.find("/el");
+    if (area == std::string::npos || area + AREA_MINI_SIZE > path.size()) {
+        path = "";
+    } else if (area + AREA_OFFSET_SIZE < path.size()) {
+        path = path.substr(area, AREA_MINI_SIZE) + "/***";
+    } else {
+        path = path.substr(area, AREA_MINI_SIZE);
     }
-    if (!noAlnumStr.empty()) {
-        noAlnum.push_back(noAlnumStr);
-        alnum.push_back("");
-    }
-    std::string res = "";
-    for (size_t i = 0; i < alnum.size(); ++i) {
-        res += (AnonyDigits(alnum[i]) + noAlnum[i]);
-    }
-    return res;
+    std::string fileName = name.substr(end + 1);
+    fileName = AnonymousName(fileName);
+    return name.substr(0, pre + PRE_OFFSET_SIZE) + "***" + path + "/" + fileName;
 }
 } // namespace DistributedData
 } // namespace OHOS
