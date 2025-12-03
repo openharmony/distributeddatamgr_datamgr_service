b4e2671ffeadc525bfa3eb39a9d0f09549678b67
liao-yonghuang <liaoyonghuang1@huawei.com>
2025-12-03 15:44:34 +0800

    支持异步创建分布式表
    Signed-off-by: liao-yonghuang <liaoyonghuang1@huawei.com>

--- services/distributeddataservice/service/test/mock/relational_store_delegate_mock.h
+++ services/distributeddataservice/service/test/mock/relational_store_delegate_mock.h
@@ -186,7 +186,8 @@ protected:
         return DBStatus::OK;
     }
 
-    DBStatus CreateDistributedTableInner(const std::string &tableName, TableSyncType type) override
+    DBStatus CreateDistributedTableInner(const std::string &tableName, TableSyncType type,
+        const CreateDistributedTableConfig &config) override
     {
         if (tableName == "test") {
             return DBStatus::DB_ERROR;
