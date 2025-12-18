038f705a7b2858f11117170169c0722ff7537992
bigteer <zhangdi126@huawei.com>
2025-12-18 16:46:20 +0800

    review update
    Signed-off-by: bigteer <zhangdi126@huawei.com>

--- services/distributeddataservice/service/rdb/rdb_flow_control_manager.cpp
+++ services/distributeddataservice/service/rdb/rdb_flow_control_manager.cpp
@@ -23,7 +23,7 @@ namespace OHOS::DistributedRdb {
 
 void RdbFlowControlManager::Init(std::shared_ptr<ExecutorPool> pool)
 {
-    if (pool == nullptr) {
+    if (pool == nullptr || pool_ != nullptr) {
         return;
     }
     pool_ = pool;
@@ -102,7 +102,7 @@ RdbFlowControlManager::Tp RdbFlowControlStrategy::GetExecuteTime(RdbFlowControlM
         uint64_t delaytime =
             std::chrono::duration_cast<std::chrono::milliseconds>(executeTime - std::chrono::steady_clock::now())
                 .count();
-        ZLOGE("Sync delay %{public}" PRIu64 "ms! Bundlename:%{public}s.", delaytime, info.label.c_str());
+        ZLOGW("Sync delay %{public}" PRIu64 "ms! bundleName:%{public}s.", delaytime, info.label.c_str());
         RdbHiViewAdapter::GetInstance().ReportRdbFault({DEVICE_SYNC,
             DEVICE_SYNC_LIMIT, info.label, "device sync delay " + std::to_string(delaytime) + " ms"});
     }
