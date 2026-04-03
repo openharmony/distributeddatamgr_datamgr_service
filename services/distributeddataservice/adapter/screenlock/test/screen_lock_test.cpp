bed3d4d06dfa0e4a3e53da14f96dd0cae5496355
zhanglele33 <zhanglele33@h-partners.com>
2026-04-03 14:18:37 +0800

    添加mock方法，调整测试用例 Co-Authored-By:Agent
    Signed-off-by: zhanglele33 <zhanglele33@h-partners.com>

--- services/distributeddataservice/adapter/screenlock/test/screen_lock_test.cpp
+++ services/distributeddataservice/adapter/screenlock/test/screen_lock_test.cpp
@@ -16,9 +16,13 @@
 #include "screenlock/screen_lock.h"
 
 #include <gtest/gtest.h>
+
+#include "common_event_manager_mock.h"
+
 namespace {
 using namespace OHOS::DistributedData;
 using namespace testing::ext;
+using namespace EventFwk;
 class ScreenLockObserver : public ScreenManager::Observer {
 public:
     void OnScreenUnlocked(int32_t user) override
@@ -43,142 +47,187 @@ class ScreenLockTest : public testing::Test {
 public:
     static void SetUpTestCase(void)
     {
+        mock_ = std::make_shared<CommonEventManagerMock>();
+        screenLock_ = std::make_shared<ScreenLock>();
     }
     static void TearDownTestCase(void)
     {
+        screenLock_ = nullptr;
+        mock_ = nullptr;
     }
-    void SetUp()
-    {
-    }
-    void TearDown()
-    {
-    }
+    void SetUp() {}
+    void TearDown() {}
+
 protected:
-    static constexpr int32_t MAX_RETRY_TIME = 300;
+    static std::shared_ptr<CommonEventManagerMock> mock_;
+    static std::shared_ptr<ScreenLock> screenLock_;
+    static constexpr int WAIT_TIMEOUT_S = 3;
 };
 
+std::shared_ptr<CommonEventManagerMock> ScreenLockTest::mock_;
+std::shared_ptr<ScreenLock> ScreenLockTest::screenLock_;
+
 /**
-* @tc.name: Subscribe001
-* @tc.desc: invalid param
-* @tc.type: FUNC
-* @tc.require:
-* @tc.author:
-*/
+ * @tc.name: Subscribe001
+ * @tc.desc: subscribe nullptr or empty name observer
+ * @tc.type: FUNC
+ */
 HWTEST_F(ScreenLockTest, Subscribe001, TestSize.Level0)
 {
-    auto screenLock = std::make_shared<ScreenLock>();
-    screenLock->Subscribe(nullptr);
-    EXPECT_TRUE(screenLock->observerMap_.Empty());
+    screenLock_->Subscribe(nullptr);
+    EXPECT_TRUE(screenLock_->observerMap_.Empty());
+
     auto observer = std::make_shared<ScreenLockObserver>();
     observer->SetName("");
-    screenLock->Subscribe(observer);
-    EXPECT_TRUE(screenLock->observerMap_.Empty());
+    screenLock_->Subscribe(observer);
+    EXPECT_TRUE(screenLock_->observerMap_.Empty());
 }
 
 /**
 * @tc.name: Subscribe002
 * @tc.desc: valid param
 * @tc.type: FUNC
-* @tc.require:
-* @tc.author:
 */
 HWTEST_F(ScreenLockTest, Subscribe002, TestSize.Level0)
 {
-    auto screenLock = std::make_shared<ScreenLock>();
     auto observer = std::make_shared<ScreenLockObserver>();
-    screenLock->Subscribe(observer);
-    EXPECT_TRUE(screenLock->observerMap_.Contains(observer->GetName()));
-    screenLock->Subscribe(observer);
-    EXPECT_EQ(screenLock->observerMap_.Size(), 1);
+    screenLock_->Subscribe(observer);
+    EXPECT_TRUE(screenLock_->observerMap_.Contains(observer->GetName()));
+
+    screenLock_->Subscribe(observer);
+    EXPECT_EQ(screenLock_->observerMap_.Size(), 1);
+
+    screenLock_->Unsubscribe(observer);
+    EXPECT_TRUE(screenLock_->observerMap_.Empty());
 }
 
 /**
-* @tc.name: Unsubscribe001
-* @tc.desc: test normal unsubscribe
-* @tc.type: FUNC
-* @tc.require:
-* @tc.author:
-*/
-HWTEST_F(ScreenLockTest, Unsubscribe001, TestSize.Level0)
-{
-    auto screenLock = std::make_shared<ScreenLock>();
-    auto observer = std::make_shared<ScreenLockObserver>();
-    screenLock->Subscribe(observer);
-    EXPECT_EQ(screenLock->observerMap_.Size(), 1);
-    screenLock->Unsubscribe(observer);
-    EXPECT_TRUE(screenLock->observerMap_.Empty());
-}
-
-/**
-* @tc.name: SubscribeScreenEvent001
-* @tc.desc: subscribe ScreenEvent
-* @tc.type: FUNC
-* @tc.author:
-*/
+ * @tc.name: SubscribeScreenEvent001
+ * @tc.desc: cover all branches: eventSubscriber_ null/not null, executors_ null/not null
+ * @tc.type: FUNC
+ */
 HWTEST_F(ScreenLockTest, SubscribeScreenEvent001, TestSize.Level0)
 {
-    auto executor = std::make_shared<OHOS::ExecutorPool>(12, 5);
-    auto screenLock = std::make_shared<ScreenLock>();
-    screenLock->BindExecutor(executor);
-    ASSERT_NE(screenLock->executors_, nullptr);
-    EXPECT_EQ(screenLock->eventSubscriber_, nullptr);
-    screenLock->SubscribeScreenEvent();
-    EXPECT_NE(screenLock->eventSubscriber_, nullptr);
-    screenLock->UnsubscribeScreenEvent();
-    screenLock->executors_ = nullptr;
+    EXPECT_EQ(screenLock_->eventSubscriber_, nullptr);
+    EXPECT_EQ(screenLock_->executors_, nullptr);
+    EXPECT_CALL(*mock_, SubscribeCommonEvent(_)).Times(0);
+    screenLock_->SubscribeScreenEvent();
+    EXPECT_NE(screenLock_->eventSubscriber_, nullptr);
+
+    auto executor = std::make_shared<ExecutorPool>(1, 0);
+    screenLock_->BindExecutor(executor);
+
+    bool subscribed = false;
+    EXPECT_CALL(*mock_, SubscribeCommonEvent(_)).WillRepeatedly([&subscribed](auto &) {
+        subscribed = true;
+        return true;
+    });
+    screenLock_->SubscribeScreenEvent();
+    int elapsed = 0;
+    while (!subscribed && elapsed < WAIT_TIMEOUT_S) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        elapsed++;
+    }
+    ASSERT_TRUE(subscribed);
+    screenLock_->executors_ = nullptr;
 }
 
 /**
-* @tc.name: SubscribeScreenEvent002
-* @tc.desc: subscribe ScreenEvent
-* @tc.type: FUNC
-* @tc.author:
-*/
-HWTEST_F(ScreenLockTest, SubscribeScreenEvent002, TestSize.Level0)
+ * @tc.name: UnsubscribeScreenEvent001
+ * @tc.desc: unsubscribe screen event success
+ * @tc.type: FUNC
+ */
+HWTEST_F(ScreenLockTest, UnsubscribeScreenEvent001, TestSize.Level0)
 {
-    auto screenLock = std::make_shared<ScreenLock>();
-    EXPECT_EQ(screenLock->executors_, nullptr);
-    EXPECT_EQ(screenLock->eventSubscriber_, nullptr);
-    screenLock->SubscribeScreenEvent();
-    EXPECT_NE(screenLock->eventSubscriber_, nullptr);
-    screenLock->UnsubscribeScreenEvent();
+    screenLock_->SubscribeScreenEvent();
+    bool called = false;
+    EXPECT_CALL(*mock_, UnSubscribeCommonEvent(_)).WillOnce([&called](auto &) {
+        called = true;
+        return true;
+    });
+    screenLock_->UnsubscribeScreenEvent();
+    ASSERT_TRUE(called);
 }
 
 /**
-* @tc.name: GetTask001
-* @tc.desc: subscribe ScreenEvent
-* @tc.type: FUNC
-* @tc.author:
-*/
+ * @tc.name: UnsubscribeScreenEvent002
+ * @tc.desc: unsubscribe screen event fail
+ * @tc.type: FUNC
+ */
+HWTEST_F(ScreenLockTest, UnsubscribeScreenEvent002, TestSize.Level0)
+{
+    screenLock_->SubscribeScreenEvent();
+    bool called = false;
+    EXPECT_CALL(*mock_, UnSubscribeCommonEvent(_)).WillOnce([&called](auto &) {
+        called = true;
+        return false;
+    });
+    screenLock_->UnsubscribeScreenEvent();
+    ASSERT_TRUE(called);
+}
+
+/**
+ * @tc.name: GetTask001
+ * @tc.desc: subscribe fail, executors_ null, no retry
+ * @tc.type: FUNC
+ */
 HWTEST_F(ScreenLockTest, GetTask001, TestSize.Level0)
 {
-    auto screenLock = std::make_shared<ScreenLock>();
-    EXPECT_EQ(screenLock->executors_, nullptr);
-    EXPECT_EQ(screenLock->eventSubscriber_, nullptr);
-    auto Task = screenLock->GetTask(0);
-    Task();
-    auto executor = std::make_shared<OHOS::ExecutorPool>(12, 5);
-    screenLock->BindExecutor(executor);
-    executor = nullptr;
-    ASSERT_NE(screenLock->executors_, nullptr);
-    Task = screenLock->GetTask(MAX_RETRY_TIME - 1);
-    Task();
+    bool subscribed = false;
+    EXPECT_CALL(*mock_, SubscribeCommonEvent(_)).WillRepeatedly([&subscribed](auto &) {
+        subscribed = true;
+        return false;
+    });
+    EXPECT_EQ(screenLock_->executors_, nullptr);
+    screenLock_->GetTask(0)();
+    ASSERT_TRUE(subscribed);
 }
 
 /**
-* @tc.name: GetTask002
-* @tc.desc: subscribe ScreenEvent
-* @tc.type: FUNC
-* @tc.author:
-*/
+ * @tc.name: GetTask002
+ * @tc.desc: subscribe fail, retry exceeded, no retry
+ * @tc.type: FUNC
+ */
 HWTEST_F(ScreenLockTest, GetTask002, TestSize.Level0)
 {
-    auto screenLock = std::make_shared<ScreenLock>();
-    EXPECT_EQ(screenLock->executors_, nullptr);
-    EXPECT_EQ(screenLock->eventSubscriber_, nullptr);
-    screenLock->SubscribeScreenEvent();
-    EXPECT_NE(screenLock->eventSubscriber_, nullptr);
-    auto Task = screenLock->GetTask(0);
-    Task();
+    bool subscribed = false;
+    EXPECT_CALL(*mock_, SubscribeCommonEvent(_)).WillRepeatedly([&subscribed](auto &) {
+        subscribed = true;
+        return false;
+    });
+    auto executor = std::make_shared<ExecutorPool>(1, 0);
+    screenLock_->BindExecutor(executor);
+    screenLock_->GetTask(ScreenLock::MAX_RETRY_TIMES)();
+    ASSERT_TRUE(subscribed);
+    screenLock_->executors_ = nullptr;
+}
+
+/**
+ * @tc.name: GetTask003
+ * @tc.desc: subscribe fail, has executor, schedule retry
+ * @tc.type: FUNC
+ */
+HWTEST_F(ScreenLockTest, GetTask003, TestSize.Level0)
+{
+    bool scheduled = false;
+    bool retried = false;
+    EXPECT_CALL(*mock_, SubscribeCommonEvent(_)).WillRepeatedly([&](auto &) {
+        if (scheduled) {
+            retried = true;
+            return false;
+        }
+        scheduled = true;
+        return false;
+    });
+    auto executor = std::make_shared<ExecutorPool>(1, 0);
+    screenLock_->BindExecutor(executor);
+    screenLock_->GetTask(ScreenLock::MAX_RETRY_TIMES - 1)();
+    int elapsed = 0;
+    while (!retried && elapsed < WAIT_TIMEOUT_S) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        elapsed++;
+    }
+    ASSERT_TRUE(retried);
+    screenLock_->executors_ = nullptr;
 }
 } // namespace
