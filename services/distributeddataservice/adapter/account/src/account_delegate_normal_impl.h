3077f84c6d9f8e9b5c96b3c29044f4f96924376a
zqq_ddd <zhangqiquan1@huawei.com>
2026-07-21 11:33:42 +0800

    revert login account check
    Signed-off-by: zqq_ddd <zhangqiquan1@huawei.com>

--- services/distributeddataservice/adapter/account/src/account_delegate_normal_impl.h
+++ services/distributeddataservice/adapter/account/src/account_delegate_normal_impl.h
@@ -42,7 +42,6 @@ public:
     bool QueryUsers(std::vector<int> &users) override;
     bool QueryForegroundUsers(std::vector<int> &users) override;
     bool IsLoginAccount() override;
-    bool IsLoginAccount(int32_t userId) override;
     bool IsVerified(int userId) override;
     bool IsDeactivating(int userId) override;
     void SubscribeAccountEvent() override;
